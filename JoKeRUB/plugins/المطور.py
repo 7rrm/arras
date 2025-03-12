b6d46598cfb92d12b47ed4bbe43a19c78d33e946
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-03-12 20:48:05 +0300

    Update and rename المطور (1).py to المطور.py

--- JoKeRUB/plugins/المطور.py
+++ JoKeRUB/plugins/المطور.py
@@ -50,14 +50,14 @@ async def amireallyalive(event):
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "  - "
     CUSTOM_ALIVE_TEXT = gvarstatus("ALIVE_TEXT")
-    CAT_IMG = " https://telegra.ph/file/83d3c9610bc957b20e35a.jpg "
+    CAT_IMG = " https://graph.org/file/a467d3702fbc9ae391fe0-e6322ec96a2fd4c1f4.jpg "
     if CAT_IMG:
         CAT = [x for x in CAT_IMG.split()]
         A_IMG = list(CAT)
         PIC = random.choice(A_IMG)
-        cat_caption = f"مطورين الجوكر\n"
+        cat_caption = f"الـمطورين\n"
         cat_caption += f"✛━━━━━━━━━━━━━✛\n"
-        cat_caption += f"- المطور  : @lMl10l\n"
+        cat_caption += f"- المطور  : @Lx5x5\n"
         cat_caption += f"✛━━━━━━━━━━━━━✛\n"
         await event.client.send_file(
             event.chat_id, PIC, caption=cat_caption, reply_to=reply_to_id
@@ -68,7 +68,7 @@ async def on_plug_in_callback_query_handler(event):
     statstext = await catalive(StartTime)
     await event.answer(statstext, cache_time=0, alert=True)
 
-progs = [1374312239, 393120911, 705475246, 5564802580]
+progs = [5427469031, 393120911]
 
 @l313l.on(events.NewMessage(incoming=True))
 async def reda(event):

ca8b80fe7281006990130a9ea23e270d4451f860
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-03-21 00:20:34 +0300

    Update زغرفة.py

--- JoKeRUB/plugins/زغرفة.py
+++ JoKeRUB/plugins/زغرفة.py
@@ -1,77 +1,268 @@
-import asyncio
-import requests
-import json
-from asyncio import sleep
-from telethon import events, types
-from ..core.managers import edit_delete, edit_or_reply
-from ..helpers.utils import reply_id
-from . import l313l
+import random
 
-@l313l.ar_cmd(pattern="زخرفه(?: |$)(.*)")
-async def zelzal_gif(event):
-    namz = event.pattern_match.group(1)
-    reply = await event.get_reply_message()
-    if not namz and reply:
-        return await edit_delete(event, "**- ارسـل (.زخرفه) + اسمـك بالانكلـش**", 10)
-    if not namz:
-        return await edit_delete(event, "**- ارسـل (.زخرفه) + اسمـك بالانكلـش**", 10)
-    data = {
-        'text': namz,
-        '_csrf': '',
-        'pages[]': [
-            'New',
-            'Unique',
-            'CoolText',
-        ],
-    }
-    response = requests.post('https://www.fancytextpro.com/generate', headers=headers, data=data)
-    data = json.loads(response.content)
-    #s1 = data['MusicalMap']
-    s2 = data['neonCharMap']
-    s3 = data['boldCharMap']
-    s4 = data['EmojiMap']
-    s4 = data['italicCharMap']
-    s5 = data['AncientMap']
-    s6 = data['Ladyleo']
-    if "💋" in s6:
-        s6 = s6.replace("💋 ", "").replace(" 💋", "")
-    s7 = data['boldItalicCharMap']
-    s8 = data['SinoTibetan']
-    s9 = data['monospaceCharMap']
-    s10 = data['weirdChar']
-    s11 = data['BoldFloara']
-    if "🌸" in s11:
-        s11 = s11.replace("🌸ꗥ～ꗥ🌸 ", "ꗥ～").replace(" 🌸ꗥ～ꗥ🌸", "～ꗥ")
-    s12 = data['upperAnglesCharMap']
-    s13 = data['BuzzChar']
-    s14 = data['greekCharMap']
-    s15 = data['SunnyDay']
-    s16 = data['invertedSquaresCharMap']
-    if "🅰" in s16:
-        s16 = s16.replace("🅰", "🅐")
-    if "🅱" in s16:
-        s16 = s16.replace("🅱", "🅑")
-    if "🅿" in s16:
-        s16 = s16.replace("🅿", "🅟")
-    if "🅾" in s16:
-        s16 = s16.replace("🅾", "🅞")
-    s17 = data['TextDecorated']
-    s18 = data['doubleStruckCharMap']
-    s19 = data['Dessert']
-    s20 = data['oldEnglishCharMap']
-    s21 = data['taiVietCharMap']
-    s22 = data["oldEnglishCharBoldMap"]
-    s23 = data['oldItalicText']
-    s24 = data['cursiveLetters']
-    s25 = data['cursiveLettersBold']
-    s26 = data['BoldJavaneseText']
-    s27 = data['wideTextCharMap']
-    s28 = data['subscriptCharMap']
-    s29 = data['GunText']
-    s30 = data['superscriptCharMap']
-    s31 = data['ak47GunText']
-    zz = "زخرفـه انكلـش\n⋆┄─┄─┄─┄┄─┄─┄─┄─┄┄⋆"
-    aa = f"`{s2}`\n`{s3}`\n`{s4}`\n`{s5}`\n`{s6}`\n`{s7}`\n`{s8}`\n`{s9}`\n`{s10}`\n`{s11}`\n`{s12}`\n`{s13}`\n`{s14}`\n`{s15}`\n`{s16}`\n`{s17}`\n`{s18}`\n`{s19}`\n`{s20}`\n`{s21}`\n`{s22}`\n`{s23}`\n`{s24}`\n`{s25}`\n`{s26}`\n`{s27}`\n`{s28}`\n`{s29}`\n`{s30}`\n`{s31}`"
-    dd = "࿐  𖣳  𓃠  𖡟  𖠜  ‌♡⁩  ‌༗  ‌𖢖  ❥  ‌ঌ  𝆹𝅥𝅮  𖠜\n𖠲  𖤍  𖠛  𝅘𝅥𝅮  ‌༒  ‌ㇱ  ߷  メ 〠  𓃬  𖠄\n⋆┄─┄─┄─┄┄─┄─┄─┄─┄┄⋆\nl⌭l♥️🧸 زخرفـة انكلـش 30 نـوع تمبلـر -\n⋆┄─┄─┄─┄┄─┄─┄─┄─┄┄⋆"
-    await edit_or_reply(event, f"**{zz}**\n{aa}\n\n**{dd}**")
-    
+from JoKeRUB import l313l
+
+from ..core.managers import edit_or_reply
+from . import fonts
+
+plugin_category = "extra"
+
+
+@l313l.ar_cmd(
+    pattern="زغرفة1(?:\s|$)([\s\S]*)",
+    command=("زغرفة1", plugin_category),)
+async def stylish_generator(event):
+    "Changes font style of the given text"
+    args = event.pattern_match.group(1)
+    if not args:
+        get = await event.get_reply_message()
+        args = get.text
+    if not args:
+        await edit_or_reply(event, "**اعـطنـي نـص اولا 🧸🖤**")
+        return
+    string = "  ".join(args).lower()
+    for normalfontcharacter in string:
+        if normalfontcharacter in fonts.normalfont:
+            musicalcharacter = fonts.musicalfont[
+                fonts.normalfont.index(normalfontcharacter)
+            ]
+            string = string.replace(normalfontcharacter, musicalcharacter)
+    await edit_or_reply(event, string)
+
+
+@l313l.ar_cmd(
+    pattern="زغرفة2(?:\s|$)([\s\S]*)",
+    command=("زغرفة2", plugin_category),)
+async def stylish_generator(event):
+    "Changes font style of the given text"
+    args = event.pattern_match.group(1)
+    if not args:
+        get = await event.get_reply_message()
+        args = get.text
+    if not args:
+        await edit_or_reply(event, "**اعـطنـي نـص اولا 🌺😗🗿**")
+        return
+    string = "  ".join(args).lower()
+    for normalfontcharacter in string:
+        if normalfontcharacter in fonts.normalfont:
+            ancientcharacter = fonts.ancientfont[
+                fonts.normalfont.index(normalfontcharacter)
+            ]
+            string = string.replace(normalfontcharacter, ancientcharacter)
+    await edit_or_reply(event, string)
+
+
+@l313l.ar_cmd(
+    pattern="زغرفة3(?:\s|$)([\s\S]*)",
+    command=("زغرفة3", plugin_category),)
+async def vapor(event):
+    "Changes font style of the given text"
+    reply_text = []
+    textx = await event.get_reply_message()
+    message = event.pattern_match.group(1)
+    if message:
+        pass
+    elif textx:
+        message = textx.text
+    else:
+        await edit_or_reply(event, "اعـطنـي نـص اولا 🧸♥")
+        return
+
+    for charac in message:
+        if 0x21 <= ord(charac) <= 0x7F:
+            reply_text.append(chr(ord(charac) + 0xFEE0))
+        elif ord(charac) == 0x20:
+            reply_text.append(chr(0x3000))
+        else:
+            reply_text.append(charac)
+
+    await edit_or_reply(event, "".join(reply_text))
+
+
+@l313l.ar_cmd(
+    pattern="زغرفة4(?:\s|$)([\s\S]*)",
+    command=("زغرفة4", plugin_category),
+)
+async def stylish_generator(event):
+    "Changes font style of the given text"
+    args = event.pattern_match.group(1)
+    if not args:
+        get = await event.get_reply_message()
+        args = get.text
+    if not args:
+        await edit_or_reply(event, "**اعـطنـي نـص اولا 🧸🖤**")
+        return
+    string = "  ".join(args).lower()
+    for normaltextcharacter in string:
+        if normaltextcharacter in fonts.normaltext:
+            smallcapscharacter = fonts.smallcapsfont[
+                fonts.normaltext.index(normaltextcharacter)
+            ]
+            string = string.replace(normaltextcharacter, smallcapscharacter)
+    await edit_or_reply(event, string)
+
+
+@l313l.ar_cmd(
+    pattern="زغرفة5(?:\s|$)([\s\S]*)",
+    command=("زغرفة5", plugin_category),
+)
+async def stylish_generator(event):
+    "Changes font style of the given text"
+    args = event.pattern_match.group(1)
+    if not args:
+        get = await event.get_reply_message()
+        args = get.text
+    if not args:
+        await edit_or_reply(event, "**اعـطنـي نـص اولا 🧸🖤**")
+        return
+    string = "  ".join(args).lower()
+    for normaltextcharacter in string:
+        if normaltextcharacter in fonts.normaltext:
+            bubblesblackcharacter = fonts.bubblesblackfont[
+                fonts.normaltext.index(normaltextcharacter)
+            ]
+            string = string.replace(normaltextcharacter, bubblesblackcharacter)
+    await edit_or_reply(event, string)
+
+
+@l313l.ar_cmd(
+    pattern="زغرفة6(?:\s|$)([\s\S]*)",
+    command=("زغرفة6", plugin_category),
+)
+async def stylish_generator(event):
+    "Changes font style of the given text"
+    args = event.pattern_match.group(1)
+    if not args:
+        get = await event.get_reply_message()
+        args = get.text
+    if not args:
+        await edit_or_reply(event, "**اعـطنـي نـص اولا 🧸🖤**")
+        return
+    string = "  ".join(args).lower()
+    for normaltextcharacter in string:
+        if normaltextcharacter in fonts.normaltext:
+            bubblescharacter = fonts.bubblesfont[
+                fonts.normaltext.index(normaltextcharacter)
+            ]
+            string = string.replace(normaltextcharacter, bubblescharacter)
+    await edit_or_reply(event, string)
+
+
+@l313l.ar_cmd(
+    pattern="زغرفة7(?:\s|$)([\s\S]*)",
+    command=("زغرفة7", plugin_category),
+)
+async def stylish_generator(event):
+    "Changes font style of the given text"
+    args = event.pattern_match.group(1)
+    if not args:
+        get = await event.get_reply_message()
+        args = get.text
+    if not args:
+        await edit_or_reply(event, "**اعـطنـي نـص اولا 🧸🖤**")
+        return
+    string = "  ".join(args).lower()
+    for normaltextcharacter in string:
+        if normaltextcharacter in fonts.normaltext:
+            tantextcharacter = fonts.tantextfont[
+                fonts.normaltext.index(normaltextcharacter)
+            ]
+            string = string.replace(normaltextcharacter, tantextcharacter)
+    await edit_or_reply(event, string)
+
+
+@l313l.ar_cmd(
+    pattern="زغرفة8(?:\s|$)([\s\S]*)",
+    command=("زغرفة8", plugin_category),
+)
+async def stylish_generator(event):
+    "Changes font style of the given text"
+    args = event.pattern_match.group(1)
+    if not args:
+        get = await event.get_reply_message()
+        args = get.text
+    if not args:
+        await edit_or_reply(event, "**اعـطنـي نـص اولا 🧸🖤**")
+        return
+    string = "  ".join(args).lower()
+    for normaltextcharacter in string:
+        if normaltextcharacter in fonts.normaltext:
+            littleboxtextcharacter = fonts.littleboxtextfont[
+                fonts.normaltext.index(normaltextcharacter)
+            ]
+            string = string.replace(normaltextcharacter, littleboxtextcharacter)
+    await edit_or_reply(event, string)
+
+
+@l313l.ar_cmd(
+    pattern="زغرفة9(?:\s|$)([\s\S]*)",
+    command=("زغرفة9", plugin_category),
+)
+async def stylish_generator(event):
+    "Changes font style of the given text"
+    args = event.pattern_match.group(1)
+    if not args:
+        get = await event.get_reply_message()
+        args = get.text
+    if not args:
+        await edit_or_reply(event, "**اعـطنـي نـص اولا 🧸🖤**")
+        return
+    string = "  ".join(args).lower()
+    for normaltextcharacter in string:
+        if normaltextcharacter in fonts.normaltext:
+            hwcapitalcharacter = fonts.hwcapitalfont[
+                fonts.normaltext.index(normaltextcharacter)
+            ]
+            string = string.replace(normaltextcharacter, hwcapitalcharacter)
+    await edit_or_reply(event, string)
+
+
+@l313l.ar_cmd(
+    pattern="زغرفة10(?:\s|$)([\s\S]*)",
+    command=("زغرفة10", plugin_category),
+)
+async def stylish_generator(event):
+    "Changes font style of the given text"
+    args = event.pattern_match.group(1)
+    if not args:
+        get = await event.get_reply_message()
+        args = get.text
+    if not args:
+        await edit_or_reply(event, "**اعـطنـي نـص اولا 🧸🖤**")
+        return
+    string = "  ".join(args).lower()
+    for normaltextcharacter in string:
+        if normaltextcharacter in fonts.normaltext:
+            doubletextcharacter = fonts.doubletextfont[
+                fonts.normaltext.index(normaltextcharacter)
+            ]
+            string = string.replace(normaltextcharacter, doubletextcharacter)
+    await edit_or_reply(event, string)
+
+
+@l313l.ar_cmd(
+    pattern="زغرفة0(?:\s|$)([\s\S]*)",
+    command=("زغرفة0", plugin_category),
+)
+async def spongemocktext(mock):
+    "Changes font style of the given text"
+    reply_text = []
+    textx = await mock.get_reply_message()
+    message = mock.pattern_match.group(1)
+    if message:
+        pass
+    elif textx:
+        message = textx.text
+    else:
+        await edit_or_reply(mock, "اعـطنـي نـص اولا")
+        return
+
+    for charac in message:
+        if charac.isalpha() and random.randint(0, 1):
+            to_app = charac.upper() if charac.islower() else charac.lower()
+            reply_text.append(to_app)
+        else:
+            reply_text.append(charac)
+
+    await edit_or_reply(mock, "".join(reply_text))
+        

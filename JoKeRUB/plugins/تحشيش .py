0f2ff54860969285ff6caee75a5ede8aafb8b4c0
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-03-21 22:58:05 +0300

    Update تحشيش .py

--- JoKeRUB/plugins/تحشيش .py
+++ JoKeRUB/plugins/تحشيش .py
@@ -29,7 +29,7 @@ async def permalink(mention):
         return
     muh = user.first_name.replace("\u2060", "") if user.first_name else user.username
     rza = random.choice(roz)
-    await edit_or_reply(mention, f"نـسـبتكم انـت و [{muh}](tg://user?id={user.id}) هـي {rza} 😔🖤")
+    await edit_or_reply(mention, f"**⏎︙ نـسـبتكم انـت و [{muh}](tg://user?id={user.id}) هـي {rza}** 😔🖤")
     
     
    
@@ -46,7 +46,7 @@ async def permalink(mention):
         return await edit_or_reply(mention, f"**- لكك دي هذا المطور زلمة وعلى راسك**")
     muh = user.first_name.replace("\u2060", "") if user.first_name else user.username
     sos = random.choice(rr7)
-    await edit_or_reply(mention, f"᯽︙ نسبة الانوثة لـ [{muh}](tg://user?id={user.id}) هـي {sos} 🥵🖤")
+    await edit_or_reply(mention, f"**⏎︙ نسبة الانوثة لـ [{muh}](tg://user?id={user.id}) هـي {sos}** 🥵🖤")
 
 @l313l.ar_cmd(
     pattern="نسبة الغباء(?:\s|$)([\s\S]*)",
@@ -59,13 +59,9 @@ async def permalink(mention):
         return
     if user.id == 5427469031:
         return await edit_or_reply(mention, f"**0% ♥🙂**")
-    if user.id == 5427469031:
-        return await edit_or_reply(mention, f"**0% ♥🙂**")
-    if user.id == 5427469031:
-        return await edit_or_reply(mention, f"**0% ♥🙂**")
     muh = user.first_name.replace("\u2060", "") if user.first_name else user.username
     rzona = random.choice(rr7)
-    await edit_or_reply(mention, f"نسبة الغباء لـ [{muh}](tg://user?id={user.id}) هـي {rzona} 😂💔")
+    await edit_or_reply(mention, f"**⏎︙ نسبة الغباء لـ [{muh}](tg://user?id={user.id}) هـي {rzona}** 😂💔")
 
 @l313l.ar_cmd(
     pattern="نسبة الكذب(?:\s|$)([\s\S]*)",
@@ -80,7 +76,7 @@ async def permalink(mention):
         return await edit_or_reply(mention, f"**0% ♥🙂**")
     muh = user.first_name.replace("\u2060", "") if user.first_name else user.username
     rzona = random.choice(rr7)
-    await edit_or_reply(mention, f"نسبة الكذب لـ [{muh}](tg://user?id={user.id}) هـي {rzona} 😂💔")
+    await edit_or_reply(mention, f"**⏎︙ نسبة الكذب لـ [{muh}](tg://user?id={user.id}) هـي {rzona}** 😂💔")
 
 @l313l.ar_cmd(
     pattern="نسبة الذكاء(?:\s|$)([\s\S]*)",
@@ -95,7 +91,7 @@ async def permalink(mention):
         return await edit_or_reply(mention, f"**100% ميحتاج تسوي نسبة الذكاء للمطور معروف**")
     muh = user.first_name.replace("\u2060", "") if user.first_name else user.username
     rzona = random.choice(rr7)
-    await edit_or_reply(mention, f"نسبة الذكاء لـ [{muh}](tg://user?id={user.id}) هـي {rzona}🎈🧸")
+    await edit_or_reply(mention, f"**⏎︙ نسبة الذكاء لـ [{muh}](tg://user?id={user.id}) هـي {rzona}** 🎈🧸")
 
 @l313l.ar_cmd(
     pattern="نسبة الشذوذ(?:\s|$)([\s\S]*)",
@@ -110,7 +106,7 @@ async def permalink(mention):
         return await edit_or_reply(mention, f"**تاج راسك مطوري**")
     muh = user.first_name.replace("\u2060", "") if user.first_name else user.username
     rzona = random.choice(rr7)
-    await edit_or_reply(mention, f"نسبة الشذوذ 🏳️‍🌈 لـ [{muh}](tg://user?id={user.id}) هـي {rzona}🎈🧸")
+    await edit_or_reply(mention, f"**⏎︙ نسبة الشذوذ 🏳️‍🌈 لـ [{muh}](tg://user?id={user.id}) هـي {rzona}** 🎈🧸")
 
 @l313l.ar_cmd(
     pattern="نسبة الدياثه(?:\s|$)([\s\S]*)",
@@ -125,7 +121,7 @@ async def permalink(mention):
         return await edit_or_reply(mention, f"**تاج راسك مطوري**")
     muh = user.first_name.replace("\u2060", "") if user.first_name else user.username
     rzona = random.choice(rr7)
-    await edit_or_reply(mention, f"نسبة الدياثه لـ [{muh}](tg://user?id={user.id}) هـي {rzona}🎈🧸")
+    await edit_or_reply(mention, f"**⏎︙ نسبة الدياثه لـ [{muh}](tg://user?id={user.id}) هـي {rzona}** 🎈🧸")
 
 @l313l.ar_cmd(
     pattern="نسبة الخيانه(?:\s|$)([\s\S]*)",
@@ -140,7 +136,7 @@ async def permalink(mention):
         return await edit_or_reply(mention, f"**السيد ميخون يبقى وفي للكل**")
     muh = user.first_name.replace("\u2060", "") if user.first_name else user.username
     rzona = random.choice(rr7)
-    await edit_or_reply(mention, f"نسبة الخيانه 🙎🏼‍♀️ لـ [{muh}](tg://user?id={user.id}) هـي {rzona}🎈🧸")
+    await edit_or_reply(mention, f"**⏎︙ نسبة الخيانه 🙎🏼‍♀️ لـ [{muh}](tg://user?id={user.id}) هـي {rzona}** 🎈🧸")
 
 @l313l.ar_cmd(
     pattern="نسبة الجمال(?:\s|$)([\s\S]*)",
@@ -155,5 +151,4 @@ async def permalink(mention):
         return await edit_or_reply(mention, f"**السيد حلو ميحتاج تقيمه 🤤**")
     muh = user.first_name.replace("\u2060", "") if user.first_name else user.username
     rzona = random.choice(rr7)
-    await edit_or_reply(mention, f"نسبة جماله 👩🏻‍🦳🧑🏻 لـ [{muh}](tg://user?id={user.id}) هـي {rzona}🎈🧸")
-
+    await edit_or_reply(mention, f"**⏎︙ نسبة جماله 👩🏻‍🦳🧑🏻 لـ [{muh}](tg://user?id={user.id}) هـي {rzona}** 🎈🧸")

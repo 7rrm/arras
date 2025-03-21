e6ca45b4b64fbf4b724eb619b6a7df061dfe6264
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-03-21 11:05:37 +0300

    Update كشف (1).py

--- JoKeRUB/plugins/كشف (1).py
+++ JoKeRUB/plugins/كشف (1).py
@@ -72,7 +72,23 @@ class CustomParseMode:
     """
     def __init__(self, parse_mode: str):
         self.parse_mode = parse_mode
+        
+def parse(self, text):
+        if self.parse_mode == 'markdown':
+            text, entities = markdown.parse(text)
+        elif self.parse_mode == 'html':
+            text, entities = html.parse(text)
+        else:
+            raise InvalidFormatException("Invalid parse mode. Choose either Markdown or HTML.")
 
+        for i, e in enumerate(entities):
+            if isinstance(e, types.MessageEntityTextUrl):
+                if e.url == 'spoiler':
+                    entities[i] = types.MessageEntitySpoiler(e.offset, e.length)
+                elif e.url.startswith('emoji/'):
+                    entities[i] = types.MessageEntityCustomEmoji(e.offset, e.length, int(e.url.split('/')[1]))
+        return text, entities
+    
 async def fetch_zelzal(user_id):
     headers = {
         'Host': 'restore-access.indream.app',

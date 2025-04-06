2d06ac1de9153dc08242144eb83a66d78b6ecf76
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-04-06 14:00:10 +0300

    Update secret.py

--- JoKeRUB/assistant/secret.py
+++ JoKeRUB/assistant/secret.py
@@ -3,26 +3,39 @@ import os
 import re
 
 from telethon.events import CallbackQuery
+from telethon.tl.functions.users import GetUsersRequest
 
 from JoKeRUB import l313l
+from ..Config import Config
+from ..sql_helper.globals import gvarstatus
 
-
-@l313l.tgbot.on(CallbackQuery(data=re.compile(b"rzan_(.*)")))
+# Updated by ZThon <https://t.me/ZThon>
+@l313l.tgbot.on(CallbackQuery(data=re.compile(b"secret_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
-    if os.path.exists("./JoKeRUB/secrets.txt"):
-        jsondata = json.load(open("./JoKeRUB/secrets.txt"))
+    uzerid = gvarstatus("hmsa_id")
+    ussr = int(uzerid) if uzerid.isdigit() else uzerid
+    myid = Config.OWNER_ID
+    try:
+        zzz = await l313l.get_entity(ussr)
+    except ValueError:
+        zzz = await l313l(GetUsersRequest(ussr))
+    #user_id = event.query.user_id
+    user_id = int(uzerid)
+    file_name = f"./JoKeRUB/{user_id}.txt"
+    if os.path.exists(file_name):
+        jsondata = json.load(open(file_name))
         try:
             message = jsondata[f"{timestamp}"]
             userid = message["userid"]
-            ids = [userid, l313l.uid]
+            ids = [userid, myid, zzz.id]
             if event.query.user_id in ids:
                 encrypted_tcxt = message["text"]
                 reply_pop_up_alert = encrypted_tcxt
             else:
-                reply_pop_up_alert = "⌯︙عـذرا هذه الهـمسة ليست مخصصة لـك"
+                reply_pop_up_alert = "مطـي الهمسـه مـو الك 🧑🏻‍🦯🦓"
         except KeyError:
-            reply_pop_up_alert = "⌯︙عـذرا هذه الهمسة لم تعد موجوده في سيـرفرات جـيبثون"
+            reply_pop_up_alert = "- عـذراً .. الهمسة ليست موجهة لك !!"
     else:
-        reply_pop_up_alert = "⌯︙عـذرا هذه الهمسة لم تعد موجوده  "
+        reply_pop_up_alert = "- عـذراً .. هذه الرسـالة لم تعد موجـوده في سيـرفرات زدثــون"
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)

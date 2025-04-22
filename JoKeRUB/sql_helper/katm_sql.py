03863ecf19a7dbca4a0ed9031db1153d02b44dbf
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-04-23 01:59:59 +0300

    Update katm_sql.py

--- JoKeRUB/sql_helper/katm_sql.py
+++ JoKeRUB/sql_helper/katm_sql.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, Numeric, String, UnicodeText, Integer
+from sqlalchemy import Column, Numeric, String, UnicodeText
+
 from . import BASE, SESSION
+
 
 class Katm(BASE):
     __tablename__ = "zedkatms"
@@ -7,18 +9,23 @@ class Katm(BASE):
     ktm_id = Column(String(14), primary_key=True, nullable=False)
     f_name = Column(UnicodeText)
     f_reason = Column(UnicodeText)
-    is_temporary = Column(Integer, default=0)  # 0 for permanent, 1 for temporary
-    mute_time = Column(String(50), default="")  # For temporary mutes
 
-    def __init__(self, chat_id, ktm_id, f_name, f_reason, is_temporary=0, mute_time=""):
+    def __init__(self, chat_id, ktm_id, f_name, f_reason):
         self.chat_id = str(chat_id)
         self.ktm_id = str(ktm_id)
         self.f_name = f_name
         self.f_reason = f_reason
-        self.is_temporary = is_temporary
-        self.mute_time = mute_time
+
+    def __eq__(self, other):
+        return bool(
+            isinstance(other, Katm)
+            and self.chat_id == other.chat_id
+            and self.ktm_id == other.ktm_id
+        )
+
 
 Katm.__table__.create(bind=SESSION.get_bind(), checkfirst=True)
+
 
 def get_katm(chat_id, ktm_id):
     try:
@@ -26,31 +33,29 @@ def get_katm(chat_id, ktm_id):
     finally:
         SESSION.close()
 
-def get_katms(chat_id, is_temporary=None):
+
+def get_katms(chat_id):
     try:
-        if is_temporary is None:
-            return SESSION.query(Katm).filter(Katm.chat_id == str(chat_id)).all()
-        return SESSION.query(Katm).filter(
-            Katm.chat_id == str(chat_id),
-            Katm.is_temporary == is_temporary
-        ).all()
+        return SESSION.query(Katm).filter(Katm.chat_id == str(chat_id)).all()
     finally:
         SESSION.close()
 
-def add_katm(chat_id, ktm_id, f_name, f_reason, is_temporary=0, mute_time=""):
+
+def add_katm(chat_id, ktm_id, f_name, f_reason):
     to_check = get_katm(chat_id, ktm_id)
     if not to_check:
-        adder = Katm(str(chat_id), str(ktm_id), f_name, f_reason, is_temporary, mute_time)
+        adder = Katm(str(chat_id), str(ktm_id), f_name, f_reason)
         SESSION.add(adder)
         SESSION.commit()
         return True
     rem = SESSION.query(Katm).get((str(chat_id), str(ktm_id)))
     SESSION.delete(rem)
     SESSION.commit()
-    adder = Katm(str(chat_id), str(ktm_id), f_name, f_reason, is_temporary, mute_time)
+    adder = Katm(str(chat_id), str(ktm_id), f_name, f_reason)
     SESSION.add(adder)
     SESSION.commit()
     return False
+
 
 def remove_katm(chat_id, ktm_id):
     to_check = get_katm(chat_id, ktm_id)
@@ -61,14 +66,9 @@ def remove_katm(chat_id, ktm_id):
     SESSION.commit()
     return True
 
-def remove_all_katms(chat_id, is_temporary=None):
-    if is_temporary is None:
-        saved_katm = SESSION.query(Katm).filter(Katm.chat_id == str(chat_id))
-    else:
-        saved_katm = SESSION.query(Katm).filter(
-            Katm.chat_id == str(chat_id),
-            Katm.is_temporary == is_temporary
-        )
-    if saved_katm:
+
+def remove_all_katms(chat_id):
+    if saved_katm := SESSION.query(Katm).filter(Katm.chat_id == str(chat_id)):
         saved_katm.delete()
         SESSION.commit()
+        

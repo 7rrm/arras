3cf487c9ae5cadcff79b2425be12ac3899662cf4
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-04-22 15:12:44 +0300

    Update katm_sql.py

--- JoKeRUB/sql_helper/katm_sql.py
+++ JoKeRUB/sql_helper/katm_sql.py
@@ -15,13 +15,6 @@ class Katm(BASE):
         self.f_name = f_name
         self.f_reason = f_reason
 
-    def __eq__(self, other):
-        return bool(
-            isinstance(other, Katm)
-            and self.chat_id == other.chat_id
-            and self.ktm_id == other.ktm_id
-        )
-
 class TempKatm(BASE):
     __tablename__ = "zedtempkatms"
     chat_id = Column(String(14), primary_key=True)
@@ -42,7 +35,6 @@ class TempKatm(BASE):
     def parse_time(self, time_str):
         time_letter = time_str[-1].lower()
         time_number = int(time_str[:-1])
-        
         time_dict = {
             's': timedelta(seconds=time_number),
             'm': timedelta(minutes=time_number),
@@ -51,23 +43,10 @@ class TempKatm(BASE):
         }
         return time_dict.get(time_letter, timedelta(seconds=0))
 
-    def __eq__(self, other):
-        return bool(
-            isinstance(other, TempKatm)
-            and self.chat_id == other.chat_id
-            and self.ktm_id == other.ktm_id
-        )
-
 Katm.__table__.create(bind=SESSION.get_bind(), checkfirst=True)
 TempKatm.__table__.create(bind=SESSION.get_bind(), checkfirst=True)
 
 # دوال الكتم العام
-def get_katm(chat_id, ktm_id):
-    try:
-        return SESSION.query(Katm).get((str(chat_id), str(ktm_id)))
-    finally:
-        SESSION.close()
-
 def get_katms(chat_id):
     try:
         return SESSION.query(Katm).filter(Katm.chat_id == str(chat_id)).all()
@@ -75,41 +54,11 @@ def get_katms(chat_id):
         SESSION.close()
 
 def add_katm(chat_id, ktm_id, f_name, f_reason):
-    to_check = get_katm(chat_id, ktm_id)
-    if not to_check:
-        adder = Katm(str(chat_id), str(ktm_id), f_name, f_reason)
-        SESSION.add(adder)
-        SESSION.commit()
-        return True
-    rem = SESSION.query(Katm).get((str(chat_id), str(ktm_id)))
-    SESSION.delete(rem)
-    SESSION.commit()
     adder = Katm(str(chat_id), str(ktm_id), f_name, f_reason)
     SESSION.add(adder)
     SESSION.commit()
-    return False
-
-def remove_katm(chat_id, ktm_id):
-    to_check = get_katm(chat_id, ktm_id)
-    if not to_check:
-        return False
-    rem = SESSION.query(Katm).get((str(chat_id), str(ktm_id)))
-    SESSION.delete(rem)
-    SESSION.commit()
-    return True
-
-def remove_all_katms(chat_id):
-    if saved_katm := SESSION.query(Katm).filter(Katm.chat_id == str(chat_id)):
-        saved_katm.delete()
-        SESSION.commit()
 
 # دوال الكتم المؤقت
-def get_tempkatm(chat_id, ktm_id):
-    try:
-        return SESSION.query(TempKatm).get((str(chat_id), str(ktm_id)))
-    finally:
-        SESSION.close()
-
 def get_tempkatms(chat_id):
     try:
         return SESSION.query(TempKatm).filter(TempKatm.chat_id == str(chat_id)).all()
@@ -117,38 +66,17 @@ def get_tempkatms(chat_id):
         SESSION.close()
 
 def add_tempkatm(chat_id, ktm_id, f_name, f_reason, mute_time):
-    to_check = get_tempkatm(chat_id, ktm_id)
-    if not to_check:
-        adder = TempKatm(str(chat_id), str(ktm_id), f_name, f_reason, mute_time)
-        SESSION.add(adder)
-        SESSION.commit()
-        return True
-    rem = SESSION.query(TempKatm).get((str(chat_id), str(ktm_id)))
-    SESSION.delete(rem)
-    SESSION.commit()
     adder = TempKatm(str(chat_id), str(ktm_id), f_name, f_reason, mute_time)
     SESSION.add(adder)
     SESSION.commit()
-    return False
 
-def remove_tempkatm(chat_id, ktm_id):
-    to_check = get_tempkatm(chat_id, ktm_id)
-    if not to_check:
-        return False
-    rem = SESSION.query(TempKatm).get((str(chat_id), str(ktm_id)))
-    SESSION.delete(rem)
-    SESSION.commit()
-    return True
-
-def remove_all_tempkatms(chat_id):
-    if saved_katm := SESSION.query(TempKatm).filter(TempKatm.chat_id == str(chat_id)):
-        saved_katm.delete()
-        SESSION.commit()
-
-def check_expired_tempkatms():
+def check_expired_tempkatms(chat_id=None):
     current_time = datetime.now().timestamp()
-    expired = SESSION.query(TempKatm).filter(TempKatm.end_time <= current_time).all()
+    query = SESSION.query(TempKatm).filter(TempKatm.end_time <= current_time)
+    if chat_id:
+        query = query.filter(TempKatm.chat_id == str(chat_id))
+    expired = query.all()
     for mute in expired:
-        remove_tempkatm(mute.chat_id, mute.ktm_id)
+        SESSION.delete(mute)
     SESSION.commit()
     return expired

2f3f3e224c0a29643c6b0edb3693acadb58522f3
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-04-25 01:39:47 +0300

    Update الفارات.py

--- JoKeRUB/plugins/الفارات.py
+++ JoKeRUB/plugins/الفارات.py
@@ -9,6 +9,7 @@ from JoKeRUB import l313l
 
 from ..Config import Config
 from ..core.managers import edit_delete, edit_or_reply
+from ..sql_helper.globals import addgvar, delgvar, gvarstatus
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 

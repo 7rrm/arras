d76ce6379633fb874da3aa3b32f162fe827ddcf5
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-04-24 13:36:34 +0300

    Update صور.py

--- JoKeRUB/plugins/صور.py
+++ JoKeRUB/plugins/صور.py
@@ -1,71 +1,145 @@
-import contextlib
+import asyncio
+import aiohttp
 import os
 import shutil
+import time
+from bs4 import BeautifulSoup
+from datetime import datetime
+from telethon.utils import guess_extension
+from urllib.parse import urlencode
 
-from telethon.errors.rpcerrorlist import MediaEmptyError
+from . import l313l
+from ..Config import Config
 
-from JoKeRUB import l313l
-
-from ..core.managers import edit_or_reply
-from ..helpers.google_image_download import googleimagesdownload
-from ..helpers.utils import reply_id
-
-plugin_category = "misc"
+ZELZAL_APP_ID = "6e65179ed1d879f3d905e28ef8803625"
 
 
-@l313l.ar_cmd(
-    pattern="صور(?: |$)(\d*)? ?([\s\S]*)",
-    command=("صور", plugin_category),
-    info={
-        "header": "Google image search.",
-        "description": "To search images in google. By default will send 3 images.you can get more images(upto 10 only by changing limit value as shown in usage and examples.",
-        "usage": ["{tr}img <1-10> <query>", "{tr}img <query>"],
-        "examples": [
-            "{tr}img 10 catuserbot",
-            "{tr}img catuserbot",
-            "{tr}img 7 catuserbot",
-        ],
-    },
-)
-async def img_sampler(event):
-    "Google image search."
-    reply_to_id = await reply_id(event)
-    if event.is_reply and not event.pattern_match.group(2):
-        query = await event.get_reply_message()
-        query = str(query.message)
-    else:
-        query = str(event.pattern_match.group(2))
-    if not query:
-        return await edit_or_reply(
-            event, "** ᯽︙ قم بكتابة النص مع الامر او بالرد على النص **"
+@l313l.ar_cmd(pattern="صور (.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    await event.edit("**╮ ❐ جـاري البحث عن الصـور  ...𓅫╰**")
+    zedthon = event.pattern_match.group(1)
+    wzed_dir = os.path.join(
+        Config.TMP_DOWNLOAD_DIRECTORY,
+        zedthon
+    )
+    if not os.path.isdir(wzed_dir):
+        os.makedirs(wzed_dir)
+    input_url = "https://bots.shrimadhavuk.me/search/"
+    headers = {"USER-AGENT": "UseTGBot"}
+    url_lst = []
+    async with aiohttp.ClientSession() as requests:
+        data = {
+            "q": zedthon,
+            "app_id": ZELZAL_APP_ID,
+            "p": "GoogleImages"
+        }
+        reponse = await requests.get(
+            input_url,
+            params=data,
+            headers=headers
         )
-    cat = await edit_or_reply(event, "** ᯽︙  جارِ البحث عن الصور انتظر قليلاً ✓ **")
-    if event.pattern_match.group(1) != "":
-        lim = int(event.pattern_match.group(1))
-        lim = min(lim, 10)
-        if lim <= 0:
-            lim = 1
-    else:
-        lim = 3
-    response = googleimagesdownload()
-    # creating list of arguments
-    arguments = {
-        "keywords": query.replace(",", " "),
-        "limit": lim,
-        "format": "jpg",
-        "no_directory": "no_directory",
-    }
-    # passing the arguments to the function
-    try:
-        paths = response.download(arguments)
-    except Exception as e:
-        return await cat.edit(f"خطأ: \n`{e}`")
-    lst = paths[0][query.replace(",", " ")]
-    try:
-        await event.client.send_file(event.chat_id, lst, reply_to=reply_to_id)
-    except MediaEmptyError:
-        for i in lst:
-            with contextlib.suppress(MediaEmptyError):
-                await event.client.send_file(event.chat_id, i, reply_to=reply_to_id)
-    shutil.rmtree(os.path.dirname(os.path.abspath(lst[0])))
-    await cat.delete()
+        response = await reponse.json()
+        for result in response["results"]:
+            if len(url_lst) > 9:
+                break
+            caption = result.get("description")
+            image_url = result.get("url")
+            image_req_set = await requests.get(image_url)
+            image_file_name = str(time.time()) + "" + guess_extension(
+                image_req_set.headers.get("Content-Type")
+            )
+            image_save_path = os.path.join(
+                wzed_dir,
+                image_file_name
+            )
+            with open(image_save_path, "wb") as f_d:
+                f_d.write(await image_req_set.read())
+            url_lst.append(image_save_path)
+    if not url_lst:
+        await event.edit(f"**- اووبـس .. لم استطـع ايجـاد صـور عـن {zedthon} ؟!**\n**- حـاول مجـدداً واكتـب الكلمـه بشكـل صحيح**")
+        return
+    await event.reply(
+        file=url_lst,
+        parse_mode="html",
+        force_document=True
+    )
+    for each_file in url_lst:
+        os.remove(each_file)
+    shutil.rmtree(wzed_dir, ignore_errors=True)
+    end = datetime.now()
+    ms = (end - start).seconds
+    await event.edit(
+        f"**- اكتمـل البحث عـن {zedthon} في {ms} ثانيـه ✓**",
+        link_preview=False
+    )
+    await asyncio.sleep(5)
+    await event.delete()
+
+
+
+@l313l.ar_cmd(pattern="خلفيات (.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    await event.edit("**╮ ❐ جـاري البحث عن خلفيـات  ...𓅫╰**")
+    zedthon = event.pattern_match.group(1)
+    wzed_dir = os.path.join(
+        Config.TMP_DOWNLOAD_DIRECTORY,
+        zedthon
+    )
+    if not os.path.isdir(wzed_dir):
+        os.makedirs(wzed_dir)
+    input_url = "https://bots.shrimadhavuk.me/search/"
+    headers = {"USER-AGENT": "UseTGBot"}
+    url_lst = []
+    async with aiohttp.ClientSession() as requests:
+        data = {
+            "q": zedthon,
+            "app_id": ZELZAL_APP_ID,
+            "p": "GoogleImages"
+        }
+        reponse = await requests.get(
+            input_url,
+            params=data,
+            headers=headers
+        )
+        response = await reponse.json()
+        for result in response["results"]:
+            if len(url_lst) > 9:
+                break
+            caption = result.get("description")
+            image_url = result.get("url")
+            image_req_set = await requests.get(image_url)
+            image_file_name = str(time.time()) + "" + guess_extension(
+                image_req_set.headers.get("Content-Type")
+            )
+            image_save_path = os.path.join(
+                wzed_dir,
+                image_file_name
+            )
+            with open(image_save_path, "wb") as f_d:
+                f_d.write(await image_req_set.read())
+            url_lst.append(image_save_path)
+    if not url_lst:
+        await event.edit(f"**- اووبـس .. لم استطـع ايجـاد خلفيـات عـن {zedthon} ؟!**\n**- حـاول مجـدداً واكتـب الكلمـه بشكـل صحيح**")
+        return
+    await event.reply(
+        file=url_lst,
+        parse_mode="html",
+        force_document=True
+    )
+    for each_file in url_lst:
+        os.remove(each_file)
+    shutil.rmtree(wzed_dir, ignore_errors=True)
+    end = datetime.now()
+    ms = (end - start).seconds
+    await event.edit(
+        f"**- اكتمـل البحث عـن {zedthon} في {ms} ثانيـه ✓**",
+        link_preview=False
+    )
+    await asyncio.sleep(5)
+    await event.delete()

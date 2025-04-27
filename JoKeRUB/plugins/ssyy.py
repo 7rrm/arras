626fb0240646ded5303157fb2a2fb1075d1b1973
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-04-27 14:38:25 +0300

    Update ssyy.py

--- JoKeRUB/plugins/ssyy.py
+++ JoKeRUB/plugins/ssyy.py
@@ -585,61 +585,6 @@ async def _(event): #Code by T.me/zzzzl1l
     except Exception as e:
         print(e)
 
-async def fix_attributes(
-    path, info_dict: dict, supports_streaming: bool = False, round_message: bool = False
-) -> list:
-    """Avoid multiple instances of an attribute."""
-    new_attributes = []
-    video = False
-    audio = False
-
-    uploader = info_dict.get("uploader", "Unknown artist")
-    duration = int(info_dict.get("duration", 0))
-    suffix = path.suffix[1:]
-    if supports_streaming and suffix != "mp4":
-        supports_streaming = True
-
-    attributes, mime_type = get_attributes(path)
-    if suffix == "mp3":
-        title = str(info_dict.get("title", info_dict.get("id", "Unknown title")))
-        audio = types.DocumentAttributeAudio(
-            duration=duration, voice=None, title=title, performer=uploader
-        )
-    elif suffix == "mp4":
-        width = int(info_dict.get("width", 0))
-        height = int(info_dict.get("height", 0))
-        for attr in attributes:
-            if isinstance(attr, types.DocumentAttributeVideo):
-                duration = duration or attr.duration
-                width = width or attr.w
-                height = height or attr.h
-                break
-        video = types.DocumentAttributeVideo(
-            duration=duration,
-            w=width,
-            h=height,
-            round_message=round_message,
-            supports_streaming=supports_streaming,
-        )
-
-    if audio and isinstance(audio, types.DocumentAttributeAudio):
-        new_attributes.append(audio)
-    if video and isinstance(video, types.DocumentAttributeVideo):
-        new_attributes.append(video)
-
-    new_attributes.extend(
-        attr
-        for attr in attributes
-        if (
-            isinstance(attr, types.DocumentAttributeAudio)
-            and not audio
-            or not isinstance(attr, types.DocumentAttributeAudio)
-            and not video
-            or not isinstance(attr, types.DocumentAttributeAudio)
-            and not isinstance(attr, types.DocumentAttributeVideo)
-        )
-    )
-    return new_attributes, mime_type
 
 
 @l313l.ar_cmd(
@@ -658,71 +603,102 @@ async def download_audio(event):
     urls = extractor.find_urls(msg)
     if not urls:
         return await edit_or_reply(event, "**- قـم بادخــال رابـط مع الامـر او بالــرد ع رابـط ليتـم التحميـل**")
+    
     zedevent = await edit_or_reply(event, "**⌔╎جـارِ التحميل انتظر قليلا ▬▭ ...**")
     reply_to_id = await reply_id(event)
+    
+    # إعدادات متقدمة لتحميل الصوت مع التعامل مع الكوكيز
+    audio_opts = {
+        'format': 'bestaudio/best',
+        'addmetadata': True,
+        'writethumbnail': True,
+        'prefer_ffmpeg': True,
+        'geo_bypass': True,
+        'nocheckcertificate': True,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '320',
+        }],
+        'outtmpl': os.path.join(Config.TEMP_DIR, '%(id)s.%(ext)s'),
+        'quiet': True,
+        'logtostderr': False,
+        'no_warnings': True,
+        'cookiefile': get_cookies_file(),
+        'ignoreerrors': True,
+        'retries': 3,
+        'fragment-retries': 3,
+        'skip-unavailable-fragments': True,
+        'extract_flat': 'in_playlist',
+    }
+    
     for url in urls:
         try:
-            vid_data = YoutubeDL({"no-playlist": True, "cookiefile": get_cookies_file()}).extract_info(
-                url, download=False
+            # محاولة التحميل مع الكوكيز أولاً
+            with YoutubeDL(audio_opts) as ydl:
+                vid_data = ydl.extract_info(url, download=False)
+                startTime = time()
+                ydl.process_info(vid_data)
+            
+            # البحث عن الملف المحمل
+            audio_file = None
+            for f in os.listdir(Config.TEMP_DIR):
+                if f.startswith(vid_data.get('id', '')) and f.endswith('.mp3'):
+                    audio_file = os.path.join(Config.TEMP_DIR, f)
+                    break
+            
+            if not audio_file:
+                return await edit_delete(zedevent, "**حدث خطأ أثناء تحويل الصوت**")
+            
+            await zedevent.edit(f"**╮ ❐ جـارِ التحضيـر للـرفع انتظـر ...𓅫╰**:\n**{vid_data.get('title', url)}**")
+            
+            # رفع الملف
+            attributes, mime_type = get_attributes(audio_file)
+            ul = io.open(audio_file, 'rb')
+            
+            # الحصول على صورة المصغرة
+            thumb_pic = await get_ytthumb(vid_data.get('id', url))
+            if thumb_pic:
+                thumb_pic = await event.client.upload_file(thumb_pic)
+            
+            uploaded = await event.client.fast_upload_file(
+                file=ul,
+                progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                    progress(
+                        d, t, zedevent, startTime,
+                        "trying to upload",
+                        file_name=os.path.basename(audio_file),
+                    )
+                ),
             )
-        except ExtractorError:
-            vid_data = {"title": url, "uploader": "Catuserbot", "formats": []}
-        startTime = time()
-        retcode = await _mp3Dl(url=url, starttime=startTime, uid="320")
-        if retcode != 0:
-            return await event.edit(str(retcode))
-        _fpath = ""
-        thumb_pic = None
-        for _path in glob.glob(os.path.join(Config.TEMP_DIR, str(startTime), "*")):
-            if _path.lower().endswith((".jpg", ".png", ".webp")):
-                thumb_pic = _path
-            else:
-                _fpath = _path
-        if not _fpath:
-            return await edit_delete(zedevent, "__Unable to upload file__")
-        await zedevent.edit(
-            f"**╮ ❐ جـارِ التحضيـر للـرفع انتظـر ...𓅫╰**:\
-            \n**{vid_data['title']}***"
-        )
-        attributes, mime_type = get_attributes(str(_fpath))
-        ul = io.open(pathlib.Path(_fpath), "rb")
-        if thumb_pic is None:
-            thumb_pic = str(
-                await pool.run_in_thread(download)(
-                    await get_ytthumb(get_yt_video_id(url))
-                )
+            ul.close()
+            
+            media = types.InputMediaUploadedDocument(
+                file=uploaded,
+                mime_type=mime_type,
+                attributes=attributes,
+                force_file=False,
+                thumb=thumb_pic,
             )
-        uploaded = await event.client.fast_upload_file(
-            file=ul,
-            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
-                progress(
-                    d,
-                    t,
-                    zedevent,
-                    startTime,
-                    "trying to upload",
-                    file_name=os.path.basename(pathlib.Path(_fpath)),
-                )
-            ),
-        )
-        ul.close()
-        media = types.InputMediaUploadedDocument(
-            file=uploaded,
-            mime_type=mime_type,
-            attributes=attributes,
-            force_file=False,
-            thumb=await event.client.upload_file(thumb_pic) if thumb_pic else None,
-        )
-        await event.client.send_file(
-            event.chat_id,
-            file=media,
-            caption=f"<b>File Name : </b><code>{vid_data.get('title', os.path.basename(pathlib.Path(_fpath)))}</code>",
-            supports_streaming=True,
-            reply_to=reply_to_id,
-            parse_mode="html",
-        )
-        for _path in [_fpath, thumb_pic]:
-            os.remove(_path)
+            
+            await event.client.send_file(
+                event.chat_id,
+                file=media,
+                caption=f"<b>File Name : </b><code>{vid_data.get('title', os.path.basename(audio_file))}</code>",
+                supports_streaming=True,
+                reply_to=reply_to_id,
+                parse_mode="html",
+            )
+            
+            # تنظيف الملفات المؤقتة
+            os.remove(audio_file)
+            if thumb_pic and os.path.exists(thumb_pic):
+                os.remove(thumb_pic)
+                
+        except Exception as e:
+            await zedevent.edit(f"**حدث خطأ أثناء التحميل:**\n`{str(e)}`")
+            return
+    
     await zedevent.delete()
     
 

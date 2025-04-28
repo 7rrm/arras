c18242fb82780cbc195141c311a37be760ddc819
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-04-28 15:39:51 +0300

    Update help.py

--- JoKeRUB/plugins/help.py
+++ JoKeRUB/plugins/help.py
@@ -21,7 +21,7 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
         await l313l.get_me()
         if query.startswith("مساعدة") and event.query.user_id == l313l.uid:
             buttons = [
-                [Button.inline("البـحـث والتحميـل 🪄", data="zdownload")],
+                [Button.inline("البـحـث والتحميـل 🪄", data="kdownload")],
                 [
                     Button.inline("السـورس 🌐", data="botvr"),
                     Button.inline("الحساب 🚹", data="acccount"),
@@ -64,3 +64,53 @@ if Config.TG_BOT_USERNAME is not None and tgbot is not None:
                 link_preview=False,
             )
             await event.answer([result] if result else None)
+
+@l313l.tgbot.on(CallbackQuery(data=re.compile(rb"kdownload")))
+@check_owner
+async def zed_help(event):
+    zelzal = "⤶ عـذراً عـزيـزي 🤷🏻‍♀\n⤶ هـذه اللوحه لا تشتغل في الخاص\n⤶ لـ إظهـار لوحـة المسـاعـدة 👇\n\n⤶ ارســل (.مساعده) في اي مجمـوعـه"
+    try:
+        await event.edit(
+            "[ᯓ 𝗭𝗧𝗵𝗼𝗻 𝗨𝘀𝗲𝗿𝗯𝗼𝘁 - اوامــر البحـث والتحميــل 🛰](t.me/ZThon) .\n\n**⎉╎اليك عـزيـزي شـࢪوحـات اوامـر البحث والتحميـل من جميـع مواقـع الـ سوشـل ميديـا :**\n\n",
+            buttons=[
+                [
+                    Button.inline("فيديو", data="vedzed"),
+                    Button.inline("بحث", data="songzed"),
+                ],
+                [
+                    Button.inline("فيس بوك", data="facebook"),
+                ],
+                [
+                    Button.inline("تحميل صوت", data="downsou"),
+                    Button.inline("تحميل فيديو", data="downved"),
+                ],
+                [
+                    Button.inline("متحركات", data="giff"),
+                    Button.inline("ملصقات", data="stickkers"),
+                ],
+                [
+                    Button.inline("يوتيوب", data="youtubb"),
+                    Button.inline("ساوند كلود", data="soundcloud"),
+                ],
+                [
+                    Button.inline("انستا", data="insta"),
+                    Button.inline("بنترست", data="pentrist"),
+                    Button.inline("تيك توك", data="tiktok"),
+                ],
+                [
+                    Button.inline("كتـاب", data="bookzzz"),
+                    Button.inline("منشور مقيد", data="savzzz"),
+                    Button.inline("ستوري", data="storyzzz"),
+                ],
+                [
+                    Button.inline("بحث قنوات + مجموعات", data="telech"),
+                ],
+                [
+                    Button.inline("بحث كلمه داخل المجموعة", data="telecg"),
+                ],
+                [Button.inline("رجوع", data="ZEDHELP")],
+            ],
+        link_preview=False)
+    except Exception:
+        await event.answer(zelzal, cache_time=0, alert=True)
+        

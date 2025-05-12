41c8ffcc5652ca7d24efe47087267db6d7fa5c04
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-05-12 03:19:44 +0300

    Update الخطوط (1).py

--- JoKeRUB/plugins/الخطوط (1).py
+++ JoKeRUB/plugins/الخطوط (1).py
@@ -4,7 +4,7 @@ from ..sql_helper.globals import addgvar, delgvar, gvarstatus
 from ..core.managers import edit_delete
 from telethon import functions
 from telethon.errors.rpcerrorlist import MessageIdInvalidError
-
+import re
 # جميع أوامر الخطوط مع التعديل الجديد للخط الغامق
 @l313l.on(admin_cmd(pattern="(خط الغامق|خط غامق)"))
 async def bold_toggle(event):

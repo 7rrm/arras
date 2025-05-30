c79dc92bc2c83661e5662419282a6c478bf2bf34
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-05-30 14:36:01 +0300

    Update ssyy.py

--- JoKeRUB/plugins/ssyy.py
+++ JoKeRUB/plugins/ssyy.py
@@ -539,7 +539,7 @@ async def search_song(event):
         # إعدادات yt-dlp مع الكوكيز
         ydl_opts = {
     # أولوية لـ m4a، ثم أي تنسيق متاح
-    "format": "bestaudio/best",  # سيختار أفضل تنسيق صوتي متاح (حتى لو لم يكن m4a)
+    "format": "bestaudio[ext=m4a]/bestaudio/best",
 # إعدادات السرعة القصوى
     "socket_timeout": 5,  # وقت انتظار أقل
     "http_chunk_size": 5242880,  # 6MB - قطع أكبر للتحميل السريع
@@ -552,7 +552,6 @@ async def search_song(event):
     "quiet": True,
     "no_warnings": True,
     "geo_bypass": True,
-    
     "cookiefile": cookies_file,
     "outtmpl": "a R R a S 🎧.m4a"  # اسم ملف ثابت مع الاحتفاظ بالامتداد
         }

1b8d3bba11e53d12b7844a130846536f76693f31
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-06-21 14:08:00 +0300

    Update ssyy.py

--- JoKeRUB/plugins/ssyy.py
+++ JoKeRUB/plugins/ssyy.py
@@ -552,7 +552,7 @@ async def search_song(event):
             "no_warnings": True,
             "geo_bypass": True,
             "cookiefile": cookies_file,
-            "outtmpl": "a R R a S 🎧.m4a"
+            "outtmpl": "a R R a S.%(ext)s"
         }
         
         # البحث في اليوتيوب

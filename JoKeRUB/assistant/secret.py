fc6b527ff98afe32860f24d230d98be6fde39678
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-08-10 11:26:49 +0300

    Update secret.py

--- JoKeRUB/assistant/secret.py
+++ JoKeRUB/assistant/secret.py
@@ -1,7 +1,7 @@
 import json
 import os
 import re
-from telethon import Button
+
 from telethon.events import CallbackQuery
 from telethon.tl.functions.users import GetUsersRequest
 
@@ -9,6 +9,7 @@ from JoKeRUB import l313l
 from ..Config import Config
 from ..sql_helper.globals import gvarstatus
 
+# Updated by ZThon <https://t.me/ZThon>
 @l313l.tgbot.on(CallbackQuery(data=re.compile(b"secret_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
@@ -19,25 +20,17 @@ async def on_plug_in_callback_query_handler(event):
         zzz = await l313l.get_entity(ussr)
     except ValueError:
         zzz = await l313l(GetUsersRequest(ussr))
+    #user_id = event.query.user_id
     user_id = int(uzerid)
     file_name = f"./JoKeRUB/{user_id}.txt"
-    
     if os.path.exists(file_name):
         jsondata = json.load(open(file_name))
         try:
             message = jsondata[f"{timestamp}"]
             userid = message["userid"]
             ids = [userid, myid, zzz.id]
-            
             if event.query.user_id in ids:
                 encrypted_tcxt = message["text"]
-                
-                # إذا كان الضغط من قبل المستقبل (غير المالك)
-                if event.query.user_id == userid and event.query.user_id != myid:
-                    # تحديث النص فقط بدون تغيير الأزرار
-                    new_text = f"✅ تم قراءة الهمسة\n\n{encrypted_tcxt}"
-                    await event.edit(new_text)
-                
                 reply_pop_up_alert = encrypted_tcxt
             else:
                 reply_pop_up_alert = "مطـي الهمسـه مـو الك 🧑🏻‍🦯🦓"
@@ -45,5 +38,4 @@ async def on_plug_in_callback_query_handler(event):
             reply_pop_up_alert = "- عـذراً .. الهمسة ليست موجهة لك !!"
     else:
         reply_pop_up_alert = "- عـذراً .. هذه الرسـالة لم تعد موجـوده ."
-    
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)

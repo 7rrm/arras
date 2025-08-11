5c3b2338144874fde0903e7e216e4e4b70083457
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-08-11 14:05:36 +0300

    Update secret.py

--- JoKeRUB/assistant/secret.py
+++ JoKeRUB/assistant/secret.py
@@ -4,12 +4,12 @@ import re
 
 from telethon.events import CallbackQuery
 from telethon.tl.functions.users import GetUsersRequest
+from telethon.tl.functions.messages import UpdateInlineBotMessageRequest
 
 from JoKeRUB import l313l
 from ..Config import Config
 from ..sql_helper.globals import gvarstatus
 
-# Updated by ZThon <https://t.me/ZThon>
 @l313l.tgbot.on(CallbackQuery(data=re.compile(b"secret_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
@@ -20,9 +20,9 @@ async def on_plug_in_callback_query_handler(event):
         zzz = await l313l.get_entity(ussr)
     except ValueError:
         zzz = await l313l(GetUsersRequest(ussr))
-    #user_id = event.query.user_id
     user_id = int(uzerid)
     file_name = f"./JoKeRUB/{user_id}.txt"
+    
     if os.path.exists(file_name):
         jsondata = json.load(open(file_name))
         try:
@@ -32,6 +32,15 @@ async def on_plug_in_callback_query_handler(event):
             if event.query.user_id in ids:
                 encrypted_tcxt = message["text"]
                 reply_pop_up_alert = encrypted_tcxt
+                
+                # تحديث الزر إذا كان المستخدم هو المستقبل
+                if event.query.user_id == userid:
+                    new_button = [[Button.switch_inline("اضغـط للـرد", query=f"secret {myid} \nهلو", same_peer=True)]]
+                    await l313l(UpdateInlineBotMessageRequest(
+                        peer=await event.get_input_chat(),
+                        id=event.query.msg_id,
+                        buttons=new_button
+                    ))
             else:
                 reply_pop_up_alert = "مطـي الهمسـه مـو الك 🧑🏻‍🦯🦓"
         except KeyError:

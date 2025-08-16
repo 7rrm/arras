efc66fb3626d26a25758614c6224ec2f4154fd51
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-08-16 11:52:22 +0300

    Update znz.py

--- JoKeRUB/plugins/znz.py
+++ JoKeRUB/plugins/znz.py
@@ -7,12 +7,10 @@ import re
 import time
 from pathlib import Path
 from uuid import uuid4
-
 from telethon import Button, types
 from telethon.errors import QueryIdInvalidError
 from telethon.events import CallbackQuery, InlineQuery
 from telethon.tl.functions.users import GetUsersRequest
-
 from . import l313l
 from ..Config import Config
 from ..helpers import reply_id
@@ -24,6 +22,7 @@ from . import mention
 LOGS = logging.getLogger(__name__)
 tr = Config.COMMAND_HAND_LER
 
+# المتغيرات
 scc = "secret"
 hmm = "همسـة"
 ymm = "يستطيـع"
@@ -37,7 +36,6 @@ ttt = "ᯓ 𝗮𝗥𝗥𝗮𝗦 𝗪𝗵𝗶𝘀𝗽𝗲𝗿 - همسـة سـر
 ddd = "💌"
 bbb = None
 
-# Copyright (C) 2023 Zilzalll . All Rights Reserved
 @l313l.tgbot.on(InlineQuery)
 async def inline_handler(event):
     builder = event.builder
@@ -52,18 +50,18 @@ async def inline_handler(event):
     full_name = gvarstatus("hmsa_name") if gvarstatus("hmsa_name") else None
     username = gvarstatus("hmsa_user") if gvarstatus("hmsa_user") else None
     zelzal = None
+    
     if gvarstatus("hmsa_user"):
-        if username.startswith("@"):
-            zelzal = gvarstatus("hmsa_user")
-        else:
-            zelzal = f"[{full_name}](tg://user?id={user_id})"
-    if query_user_id == Config.OWNER_ID or query_user_id in Config.SUDO_USERS:  # Code by T.me/zzzzl1l
+        zelzal = f"[{full_name}](tg://user?id={user_id})"
+    
+    if query_user_id == Config.OWNER_ID or query_user_id in Config.SUDO_USERS:
         malathid = Config.OWNER_ID
-    elif query_user_id == user_id: #or query_user_id == int(user_id):
+    elif query_user_id == user_id:
         malathid = user_id
     else:
         malathid = None
-    if query_user_id == Config.OWNER_ID or query_user_id in Config.SUDO_USERS:  # Code by T.me/zzzzl1l
+    
+    if query_user_id == Config.OWNER_ID or query_user_id in Config.SUDO_USERS or query_user_id == user_id:
         inf = re.compile("secret (.*) (.*)")
         match2 = re.findall(inf, query)
         if match2:
@@ -71,53 +69,76 @@ async def inline_handler(event):
             zilzal = ""
             query = query[7:]
             info_type = [hmm, ymm, fmm]
+            
             if "|" in query:
                 iris, query = query.replace(" |", "|").replace("| ", "|").split("|")
                 users = iris.split(" ")
             else:
                 user, query = query.split(" ", 1)
                 users = [user]
+            
             for user in users:
                 usr = int(gvarstatus("hmsa_id")) if gvarstatus("hmsa_id") else int(user)
                 try:
                     u = await l313l.get_entity(usr)
                 except ValueError:
                     u = await l313l(GetUsersRequest(usr))
-                if u.username:
-                    zilzal += f"@{u.username}"
-                else:
-                    zilzal += f"[{u.first_name}](tg://user?id={u.id})"
+                
+                # استخدام منشن بدلاً من اليوزر
+                zilzal += f"[{u.first_name}](tg://user?id={u.id}) "
                 user_list.append(u.id)
-                zilzal += " "
+            
             zilzal = zilzal[:-1]
             old_msg = os.path.join("./JoKeRUB", f"{user_id}.txt")
+            
             try:
                 jsondata = json.load(open(old_msg))
             except Exception:
                 jsondata = False
+            
             timestamp = int(time.time() * 2)
             new_msg = {
-                str(timestamp): {"userid": user_list, "text": query}
-            }  # Code by T.me/zzzzl1l
-            buttons = [[Button.inline(info_type[2], data=f"{scc}_{timestamp}")],[Button.switch_inline(bmm, query=f"secret {malathid} \nهلو", same_peer=True)]]
+                str(timestamp): {
+                    "userid": user_list,
+                    "text": query,
+                    "read": False,
+                    "sender_id": event.query.user_id
+                }
+            }
+            
+            text_message = (
+                "ᯓ 𝗮𝗥𝗥𝗮𝗦 𝗪𝗵𝗶𝘀𝗽𝗲𝗿 - همسـة سـريـه\n"
+                "⋆┄─┄─┄─┄┄─┄─┄─┄─┄┄⋆\n"
+                "⌔╎أضغـط الـزر بالأسفـل ⚓\n"
+                f"⌔╎لـ أࢪسـال همسـه سـريـه الى {zilzal} {ddd}"
+            )
+            
+            buttons = [
+                [Button.inline(fmm, data=f"{scc}_{timestamp}")],
+                [Button.switch_inline(bmm, query=f"secret {malathid} \nهلو", same_peer=True)]
+            ]
+            
             result = builder.article(
                 title=f"{hmm} {zilzal}",
                 description=f"{dss}",
-                text=f"{hss} {zilzal} \n**{dss}**",
+                text=text_message,
                 buttons=buttons,
                 link_preview=False,
             )
             await event.answer([result] if result else None)
+            
             if jsondata:
                 jsondata.update(new_msg)
                 json.dump(jsondata, open(old_msg, "w"))
             else:
                 json.dump(new_msg, open(old_msg, "w"))
+        
         elif string == "zelzal":
             if gvarstatus("hmsa_id"):
                 bbb = [(Button.switch_inline("اضغـط هنـا", query=("secret " + gvarstatus("hmsa_id") + " \nهلو"), same_peer=True))]
             else:
                 return
+            
             results = []
             results.append(
                 builder.article(
@@ -129,71 +150,3 @@ async def inline_handler(event):
                 ),
             )
             await event.answer(results)
-    elif query_user_id == user_id:  # Code by T.me/zzzzl1l
-        inf = re.compile("secret (.*) (.*)")
-        match2 = re.findall(inf, query)
-        if match2:
-            user_list = []
-            zilzal = ""
-            query = query[7:]
-            info_type = [hmm, ymm, fmm]
-            if "|" in query:
-                iris, query = query.replace(" |", "|").replace("| ", "|").split("|")
-                users = iris.split(" ")
-            else:
-                user, query = query.split(" ", 1)
-                users = [user]
-            for user in users:
-                usr = int(user) if user.isdigit() else user
-                try:
-                    u = await l313l.get_entity(usr)
-                except ValueError:
-                    u = await l313l(GetUsersRequest(usr))
-                if u.username:
-                    zilzal += f"@{u.username}"
-                else:
-                    zilzal += f"[{u.first_name}](tg://user?id={u.id})"
-                user_list.append(u.id)
-                zilzal += " "
-            zilzal = zilzal[:-1]
-            old_msg = os.path.join("./JoKeRUB", f"{user_id}.txt")
-            try:
-                jsondata = json.load(open(old_msg))
-            except Exception:
-                jsondata = False
-            timestamp = int(time.time() * 2)
-            new_msg = {
-                str(timestamp): {"userid": user_list, "text": query}
-            }  # Code by T.me/zzzzl1l
-            buttons = [[Button.inline(info_type[2], data=f"{scc}_{timestamp}")],[Button.switch_inline(bmm, query=f"secret {malathid} \nهلو", same_peer=True)]]
-            result = builder.article(
-                title=f"{hmm} {zilzal}",
-                description=f"{dss}",
-                text=f"{hss} {zilzal} \n**{dss}**",
-                buttons=buttons,
-                link_preview=False,
-            )
-            await event.answer([result] if result else None)
-            if jsondata:
-                jsondata.update(new_msg)
-                json.dump(jsondata, open(old_msg, "w"))
-            else:
-                json.dump(new_msg, open(old_msg, "w"))
-        elif string == "zelzal":
-            if gvarstatus("hmsa_id"):
-                bbb = [(Button.switch_inline("اضغـط هنـا", query=("secret " + gvarstatus("hmsa_id") + " \nهلو"), same_peer=True))]
-            else:
-                return
-            results = []
-            results.append(
-                builder.article(
-                    title=f"{nmm}",
-                    description=f"{mnn}",
-                    text=f"**{ttt}** {zelzal} **{ddd}**",
-                    buttons=bbb,
-                    link_preview=False,
-                ),
-            )
-            await event.answer(results)
-    else:
-        return

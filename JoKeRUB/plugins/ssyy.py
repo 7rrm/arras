9887bebc947c42092461def40d56e0fc2ed67229
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-10-22 16:01:31 +0300

    Update ssyy.py

--- JoKeRUB/plugins/ssyy.py
+++ JoKeRUB/plugins/ssyy.py
@@ -834,9 +834,9 @@ async def yoot_auto_search(event):
                 # إنشاء الكابشن مع الاقتباس والإيموجي البريميوم
                 caption = (
                     f"<blockquote>\n"
-                    f"<b>D𝑜𝑤𝑛𝑙𝑜𝑎𝑑 D𝑜𝑛𝑒 𝆹𝅥𝅮 .</b>\n"
-                    f"<b>S𝑜𝑛𝑔N𝑎𝑚𝑒 :-</b> <code>{query}</code> "
+                    f"<b>D𝑜𝑤𝑛𝑙𝑜𝑎𝑑 D𝑜𝑛𝑒 𝆹𝅥𝅮 .</b>"
                     f'<a href="emoji/5449435474164731685">❤️</a>\n'
+                    f"<b>S𝑜𝑛𝑔N𝑎𝑚𝑒 :-</b> <code>{query}</code>"
                     f"</blockquote>"
                     f"<b>↯︰By: @Lx5x5</b>"
                     f'<a href="emoji/5368338253868968009">❤️</a>\n'

b8d2688229af83be37ecbf637bcfb31991ccef46
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-11-27 21:16:21 +0300

    Update اوامر الكروب.py

--- JoKeRUB/plugins/اوامر الكروب.py
+++ JoKeRUB/plugins/اوامر الكروب.py
@@ -880,19 +880,16 @@ async def Hussein(event):
             chat = await event.get_chat()
             if chat and user_id:
                 now = datetime.now()
-                if user_id in remove_members_aljoker:
-                    # إذا طرد شخصين في أقل من 60 ثانية
-                    if (now - remove_members_aljoker[user_id]).seconds < 60:
+                if user_id in remove_admins_aljoker:
+                    if (now - remove_admins_aljoker[user_id]).seconds < 60:
                         admin_info = await event.client.get_entity(user_id)
                         joker_link = f"[{admin_info.first_name}](tg://user?id={admin_info.id})"
-                        await event.reply(f"**✧︙ تم تنزيل المشرف {joker_link} بسبب قيامه بطرد عضوين في وقت قصير 🚫**")
-                        # تنزيله من الاشراف
+                        await event.reply(f"**✧︙ تم تنزيل المشرف {joker_link} بسبب قيامه بعملية تفليش فاشلة 🤣**")
                         await event.client.edit_admin(chat, user_id, change_info=False)
-                    remove_members_aljoker.pop(user_id)
-                    remove_members_aljoker[user_id] = now
+                    remove_admins_aljoker.pop(user_id)
+                    remove_admins_aljoker[user_id] = now
                 else:
-                    remove_members_aljoker[user_id] = now
-
+                    remove_admins_aljoker[user_id] = now
 @l313l.ar_cmd(pattern="منع_التفليش", require_admin=True)
 async def Hussein_aljoker(event):
     addgvar("Mn3_Kick", True)

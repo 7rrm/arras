2b0d82891917bb63791b8f20bd621cf14575c1cf
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2025-11-27 10:16:18 +0300

    Update اوامر الكروب.py

--- JoKeRUB/plugins/اوامر الكروب.py
+++ JoKeRUB/plugins/اوامر الكروب.py
@@ -869,8 +869,9 @@ async def disable_emoji_blocker(event):
     active_joker.remove(event.chat_id)
     await event.edit("**✧︙ تم تعطيل امر منع الايموجي المُميز بنجاح ✓ **")
 
-remove_admins_aljoker = {}
+remove_members_aljoker = {}
 #الكود تمت كتابته من قبل مطورين الجوكر اذا الك نية تخمطه اذكر حقوق السورس @jepthon
+
 @l313l.on(events.ChatAction)
 async def Hussein(event):
     if gvarstatus("Mn3_Kick"):
@@ -879,16 +880,18 @@ async def Hussein(event):
             chat = await event.get_chat()
             if chat and user_id:
                 now = datetime.now()
-                if user_id in remove_admins_aljoker:
-                    if (now - remove_admins_aljoker[user_id]).seconds < 60:
+                if user_id in remove_members_aljoker:
+                    # إذا طرد شخصين في أقل من 60 ثانية
+                    if (now - remove_members_aljoker[user_id]).seconds < 60:
                         admin_info = await event.client.get_entity(user_id)
                         joker_link = f"[{admin_info.first_name}](tg://user?id={admin_info.id})"
-                        await event.reply(f"**✧︙ تم تنزيل المشرف {joker_link} بسبب قيامه بعملية تفليش فاشلة 🤣**")
+                        await event.reply(f"**✧︙ تم تنزيل المشرف {joker_link} بسبب قيامه بطرد عضوين في وقت قصير 🚫**")
+                        # تنزيله من الاشراف
                         await event.client.edit_admin(chat, user_id, change_info=False)
-                    remove_admins_aljoker.pop(user_id)
-                    remove_admins_aljoker[user_id] = now
+                    remove_members_aljoker.pop(user_id)
+                    remove_members_aljoker[user_id] = now
                 else:
-                    remove_admins_aljoker[user_id] = now
+                    remove_members_aljoker[user_id] = now
 
 @l313l.ar_cmd(pattern="منع_التفليش", require_admin=True)
 async def Hussein_aljoker(event):
@@ -898,7 +901,7 @@ async def Hussein_aljoker(event):
 @l313l.ar_cmd(pattern="سماح_التفليش", require_admin=True)
 async def Hussein_aljoker(event):
     delgvar("Mn3_Kick")
-    await event.edit("**᯽︙ تم تفعيل منع التفليش للمجموعة بنجاح ✓**")
+    await event.edit("**᯽︙ تم تعطيل منع التفليش للمجموعة بنجاح ✓**")
 
 message_counts = {}
 enabled_groups = []

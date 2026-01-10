1bbdf30e017486b53621bc2b216f5da362d31d72
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2026-01-10 18:13:10 +0300

    Update التحكم.py

--- JoKeRUB/plugins/التحكم.py
+++ JoKeRUB/plugins/التحكم.py
@@ -278,6 +278,7 @@ async def _(event):  # sourcery no-metrics
             + PLG_INFO["المجموعه"]
             + PLG_INFO["النظام"]
             + PLG_INFO["الفارات"]
+            + PLG_INFO["ssyy"]
             + PLG_INFO["المطور"]
             + ["gauth"]
             + ["greset"]

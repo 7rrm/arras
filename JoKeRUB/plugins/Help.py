5028309f5f353c5a92d0580a3c1d483ad140e551
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2026-01-13 22:19:04 +0300

    Create Help.py

---
+++ JoKeRUB/plugins/Help.py
@@ -0,0 +1,160 @@
+import asyncio
+import contextlib
+import re
+import random
+import time
+import psutil
+import html
+import shutil
+import os
+import base64
+import requests
+from requests import get
+import psutil
+from datetime import datetime
+from platform import python_version
+
+from telethon import Button, events, version
+from telethon.events import CallbackQuery
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+from telethon.tl.types import MessageEntityMentionName
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest, GetUsersRequest
+from telethon.utils import pack_bot_file_id
+from telethon.errors.rpcerrorlist import YouBlockedUserError, ChatSendMediaForbiddenError
+
+from . import StartTime, l313l, zedversion, mention
+from ..core import check_owner, pool
+from ..Config import Config
+from ..utils import Zed_Vip, Zed_Dev
+from ..helpers import reply_id
+from ..helpers.utils import _format
+from ..core.logger import logging
+from ..core.managers import edit_or_reply, edit_delete
+from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+from ..helpers.functions import zedalive, check_data_base_heal_th, get_readable_time
+from . import BOTLOG, BOTLOG_CHATID, spamwatch, mention
+
+plugin_category = "العروض"
+LOGS = logging.getLogger(__name__)
+
+
+HELP = f"**🧑🏻‍💻┊مـࢪحبـاً عـزيـزي**\n**🛂┊في قائمـة المسـاعـده والشـروحـات\n🛃┊من هنـا يمكنـك ايجـاد شـرح لكـل اوامـر السـورس**\n\n[ᯓ 𝗭𝗧𝗵𝗼𝗻 𝗨𝘀𝗲𝗿𝗯𝗼𝘁 ♥️](https://t.me/ZThon)\n\n"
+
+if Config.TG_BOT_USERNAME is not None and tgbot is not None:
+
+    @tgbot.on(events.InlineQuery)
+    @check_owner
+    async def inline_handler(event):
+        builder = event.builder
+        result = None
+        query = event.text
+        
+        if query.startswith("مساعدة"):
+            buttons = [
+                [Button.inline("البـحـث والتحميـل 🪄", data="zdownload")],
+                [
+                    Button.inline("السـورس 🌐", data="botvr"),
+                    Button.inline("الحساب 🚹", data="acccount"),
+                ],
+                [
+                    Button.inline("الإذاعـة 🏟️", data="broadcastz"),
+                ],
+                [
+                    Button.inline("الكلايـش & التخصيص 🪁", data="kalaysh"),
+                ],
+                [
+                    Button.inline("المجمـوعـة 2⃣", data="groupv2"),
+                    Button.inline("المجمـوعـة 1⃣", data="groupv1"),
+                ],
+                [
+                    Button.inline("حماية المجموعات 🛗", data="grouppro"),
+                ],
+                [
+                    Button.inline("التسليـه & التحشيش 🎃", data="funzed"),
+                ],
+                [
+                    Button.inline("المرفقـات 🪁", data="extras"),
+                    Button.inline("الادوات 💡", data="toolzed"),
+                ],
+                [
+                    Button.inline("الفـارات 🎈", data="varszed"),
+                ],
+                [
+                    Button.inline("الذكـاء الاصطنـاعـي 🛸", data="ZEDAI"),
+                ],
+                [
+                    Button.inline("السوبـرات 🎡", data="superzzz"),
+                    Button.inline("التجميـع 🛗", data="pointzzz"),
+                ],
+            ]
+            result = builder.article(
+                title="قائمة المساعدة",
+                text=HELP,
+                buttons=buttons,
+                link_preview=False,
+            )
+            await event.answer([result] if result else None)
+
+@l313l.ar_cmd(pattern="مساعدة")
+async def help(event):
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    response = await l313l.inline_query(Config.TG_BOT_USERNAME, "مساعدة")
+    await response[0].click(event.chat_id)
+    await event.delete()
+
+@l313l.tgbot.on(CallbackQuery(data=re.compile(rb"ZEDHELP")))
+@check_owner
+async def _(event):
+    butze = [
+        [Button.inline("البـحـث والتحميـل 🪄", data="zdownload")],
+        [
+            Button.inline("السـورس 🌐", data="botvr"),
+            Button.inline("الحساب 🚹", data="acccount"),
+        ],
+        [
+            Button.inline("الإذاعـة 🏟️", data="broadcastz"),
+        ],
+        [
+            Button.inline("الكلايـش & التخصيص 🪁", data="kalaysh"),
+        ],
+        [
+            Button.inline("المجمـوعـة 2⃣", data="groupv2"),
+            Button.inline("المجمـوعـة 1⃣", data="groupv1"),
+        ],
+        [
+            Button.inline("حماية المجموعات 🛗", data="grouppro"),
+        ],
+        [
+            Button.inline("التسليـه & التحشيش 🎃", data="funzed"),
+        ],
+        [
+            Button.inline("المرفقـات 🪁", data="extras"),
+            Button.inline("الادوات 💡", data="toolzed"),
+        ],
+        [
+            Button.inline("الفـارات 🎈", data="varszed"),
+        ],
+        [
+            Button.inline("الذكـاء الاصطنـاعـي 🛸", data="ZEDAI"),
+        ],
+        [
+            Button.inline("السوبـرات 🎡", data="superzzz"),
+            Button.inline("التجميـع 🛗", data="pointzzz"),
+        ],
+    ]
+    await event.edit(HELP, buttons=butze, link_preview=False)
+
+@l313l.tgbot.on(CallbackQuery(data=re.compile(rb"kalaysh")))
+@check_owner
+async def _(event):
+    await event.edit(
+        "[ᯓ 𝗭𝗧𝗵𝗼𝗻 𝗨𝘀𝗲𝗿𝗯𝗼𝘁 - اوامــر تخصيص الكلايـش 🪁](t.me/ZThon) .\n\n**⎉╎اليك عـزيـزي قنـوات تخصيص كلايـش السـورس**\n**⎉╎القنوات تحتوي على كلايش متنوعه + اوامر اضافة الكلايش**",
+        buttons=[
+            [Button.url("كلايش حماية الخاص", "https://t.me/zzkrr")],
+            [Button.url("كلايش الايدي", "https://t.me/zziddd")],
+            [Button.url("كلايش الفحص", "https://t.me/zzclll")],
+            [Button.inline("رجــوع", data="ZEDHELP")],
+        ],
+    link_preview=False)

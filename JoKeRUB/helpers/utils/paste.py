f2fc2894e4fe98a50ab67d44392e927a1d0665dd
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2026-01-30 11:16:30 +0300

    Update paste.py

--- JoKeRUB/helpers/utils/paste.py
+++ JoKeRUB/helpers/utils/paste.py
@@ -1,1 +1,110 @@
+import json
+
+import requests
+
+from ...Config import Config
+from ...core.logger import logging
+
+LOGS = logging.getLogger("JoKeRUB")
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
+    "content-type": "application/json",
+}
+
+
+async def p_paste(message, extension=None):
+    """
+    To Paste the given message/text/code to paste.pelkum.dev
+    """
+    siteurl = "https://pasty.lus.pm/api/v1/pastes"
+    data = {"content": message}
+    try:
+        response = requests.post(url=siteurl, data=json.dumps(data), headers=headers)
+    except Exception as e:
+        return {"error": str(e)}
+    if response.ok:
+        response = response.json()
+        purl = (
+            f"https://pasty.lus.pm/{response['id']}.{extension}"
+            if extension
+            else f"https://pasty.lus.pm/{response['id']}.txt"
+        )
+        try:
+            from ...core.session import l313l
+
+            await l313l.send_message(
+                Config.BOTLOG_CHATID,
+                f"**You have created a new paste in pasty bin.** Link to pasty is [here]({purl}). You can delete that paste by using this token `{response['deletionToken']}`",
+            )
+        except Exception as e:
+            LOGS.info(str(e))
+        return {
+            "url": purl,
+            "raw": f"https://pasty.lus.pm/{response['id']}/raw",
+            "bin": "Pasty",
+        }
+    return {"error": "Unable to reach pasty.lus.pm"}
+
+
+async def s_paste(message, extension="txt"):
+    """
+    To Paste the given message/text/code to spaceb.in
+    """
+    siteurl = "https://spaceb.in/api/v1/documents/"
+    try:
+        response = requests.post(
+            siteurl, data={"content": message, "extension": extension}
+        )
+    except Exception as e:
+        return {"error": str(e)}
+    if response.ok:
+        response = response.json()
+        if response["error"] != "" and response["status"] < 400:
+            return {"error": response["error"]}
+        return {
+            "url": f"https://spaceb.in/{response['payload']['id']}",
+            "raw": f"{siteurl}{response['payload']['id']}/raw",
+            "bin": "Spacebin",
+        }
+    return {"error": "Unable to reach spacebin."}
+
+
+async def n_paste(message, extension=None):
+    """
+    To Paste the given message/text/code to nekobin
+    """
+    siteurl = "https://nekobin.com/api/documents"
+    data = {"content": message}
+    try:
+        response = requests.post(url=siteurl, data=json.dumps(data), headers=headers)
+    except Exception as e:
+        return {"error": str(e)}
+    if response.ok:
+        response = response.json()
+        purl = (
+            f"nekobin.com/{response['result']['key']}.{extension}"
+            if extension
+            else f"nekobin.com/{response['result']['key']}"
+        )
+        return {
+            "url": purl,
+            "raw": f"nekobin.com/raw/{response['result']['key']}",
+            "bin": "Neko",
+        }
+    return {"error": "Unable to reach nekobin."}
+
+
+async def d_paste(message, extension=None):
+    """
+    To Paste the given message/text/code to dogbin
+    """
+    siteurl = "https://del.dog/documents"
+    data = {"content": message}
+    try:
+        response = requests.post(url=siteurl, data=json.dumps(data), headers=headers)
+    except Exception as e:
+        return {"error": str(e)}
+    if response.ok:
+        response = response.json()
 

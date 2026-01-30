ebec9ff2888db386d5d511bb0855ad1909f64a69
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2026-01-30 21:51:13 +0300

    Update paste.py

--- JoKeRUB/helpers/utils/paste.py
+++ JoKeRUB/helpers/utils/paste.py
@@ -5,7 +5,7 @@ import requests
 from ...Config import Config
 from ...core.logger import logging
 
-LOGS = logging.getLogger("Zelzal")
+LOGS = logging.getLogger("JoKeRUB")
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
@@ -31,9 +31,9 @@ async def p_paste(message, extension=None):
             else f"https://pasty.lus.pm/{response['id']}.txt"
         )
         try:
-            from ...core.session import zedub
+            from ...core.session import l313l
 
-            await zedub.send_message(
+            await l313l.send_message(
                 Config.BOTLOG_CHATID,
                 f"**You have created a new paste in pasty bin.** Link to pasty is [here]({purl}). You can delete that paste by using this token `{response['deletionToken']}`",
             )
@@ -48,29 +48,6 @@ async def p_paste(message, extension=None):
 
 
 async def s_paste(message, extension="txt"):
-    """
-    To Paste the given message/text/code to spaceb.in
-    """
-    siteurl = "https://spaceb.in/api/v1/documents/"
-    try:
-        response = requests.post(
-            siteurl, data={"content": message, "extension": extension}
-        )
-    except Exception as e:
-        return {"error": str(e)}
-    if response.ok:
-        response = response.json()
-        if response["error"] != "" and response["status"] < 400:
-            return {"error": response["error"]}
-        return {
-            "url": f"https://spaceb.in/{response['payload']['id']}",
-            "raw": f"{siteurl}{response['payload']['id']}/raw",
-            "bin": "Spacebin",
-        }
-    return {"error": "Unable to reach spacebin."}
-
-
-def spaste(message, extension="txt"):
     """
     To Paste the given message/text/code to spaceb.in
     """
@@ -122,7 +99,7 @@ async def d_paste(message, extension=None):
     """
     To Paste the given message/text/code to dogbin
     """
-    siteurl = "http://catbin.up.railway.app/documents"
+    siteurl = "https://del.dog/documents"
     data = {"content": message}
     try:
         response = requests.post(url=siteurl, data=json.dumps(data), headers=headers)
@@ -131,17 +108,16 @@ async def d_paste(message, extension=None):
     if response.ok:
         response = response.json()
         purl = (
-            f"http://catbin.up.railway.app/{response['key']}.{extension}"
+            f"https://del.dog/{response['key']}.{extension}"
             if extension
-            else f"http://catbin.up.railway.app/{response['key']}"
+            else f"https://del.dog/{response['key']}"
         )
         return {
             "url": purl,
-            "raw": f"http://catbin.up.railway.app/raw/{response['key']}",
+            "raw": f"https://del.dog/raw/{response['key']}",
             "bin": "Dog",
         }
     return {"error": "Unable to reach dogbin."}
-
 
 async def pastetext(text_to_print, pastetype=None, extension=None):
     response = {"error": "something went wrong"}
@@ -168,3 +144,4 @@ async def pastetext(text_to_print, pastetype=None, extension=None):
     if "error" in response:
         response = await d_paste(text_to_print, extension)
     return response
+    

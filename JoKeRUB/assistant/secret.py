6c5845a34f596e69fac71219573ac48046040c7b
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2026-02-12 16:48:51 +0300

    Update secret.py

--- JoKeRUB/assistant/secret.py
+++ JoKeRUB/assistant/secret.py
@@ -67,7 +67,7 @@ async def on_plug_in_callback_query_handler(event):
                     
                     new_text = f'''\
 <tg-emoji emoji-id="5933974679269151927">📨</tg-emoji> <b> تم قراءة الهمسـة </b>
-<tg-emoji emoji-id="5933974679269151927">📨</tg-emoji><b>قــرأهـا</b> <tg-emoji emoji-id="4929526216945305427">📨</tg-emoji>{receiver_name}</b> <tg-emoji emoji-id="4931832872081294660">✅</tg-emoji>
+<tg-emoji emoji-id="5933974679269151927">📨</tg-emoji><b>قــرأهـا</b> <tg-emoji emoji-id="4931832872081294660">📨</tg-emoji>{receiver_name}</b> <tg-emoji emoji-id="4929526216945305427">✅</tg-emoji>
 <tg-emoji emoji-id="5933974679269151927">📨</tg-emoji><b>عَـند</b> <code>{time_str}</code> . </b> <tg-emoji emoji-id="5839380464116175529">🕖</tg-emoji>'''
                     
                     # زر الرد يرسل همسة للمرسل الأصلي

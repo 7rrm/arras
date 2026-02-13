1a59dd33ad3e82d4aaa44764c25f9544648152a3
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2026-02-13 10:23:15 +0300

    Update تنصيب.py

--- JoKeRUB/plugins/تنصيب.py
+++ JoKeRUB/plugins/تنصيب.py
@@ -34,39 +34,3 @@ async def repo(event):
     response = await bot.inline_query(TG_BOT, "السورس")
     await response[0].click(event.chat_id)
     await event.delete()
-from telethon import events, Button
-from ..Config import Config
-from ..sql_helper.globals import gvarstatus
-from l313l.razan.resources.mybot import *
-
-ROZ_PIC = "https://graph.org/file/2e51431a290028d612377-07abd6e9a86fde6949.jpg"
-
-if Config.TG_BOT_USERNAME is not None and tgbot is not None:
-    @tgbot.on(events.InlineQuery)
-    async def inline_handler(event):
-        builder = event.builder
-        result = None
-        query = event.text
-        await bot.get_me()
-        if query.startswith("السورس") and event.query.user_id == bot.uid:
-            buttons = [
-                [Button.url("∙ المـطور ∙", "https://t.me/lx5x5")]
-            ]
-            if ROZ_PIC and ROZ_PIC.endswith((".jpg", ".png", "gif", "mp4")):
-                result = builder.photo(ROZ_PIC, text=ROZ, buttons=buttons, link_preview=False)
-            elif ROZ_PIC:
-                result = builder.document(ROZ_PIC, title="JoKeRUB", text=ROZ, buttons=buttons, link_preview=False)
-            else:
-                result = builder.article(title="JoKeRUB", text=ROZ, buttons=buttons, link_preview=False)
-            await event.answer([result] if result else None)
-
-@bot.on(admin_cmd(outgoing=True, pattern="السورس"))
-async def repo(event):
-    if event.fwd_from:
-        return
-    TG_BOT = Config.TG_BOT_USERNAME
-    if event.reply_to_msg_id:
-        await event.get_reply_message()
-    response = await bot.inline_query(TG_BOT, "السورس")
-    await response[0].click(event.chat_id)
-    await event.delete()

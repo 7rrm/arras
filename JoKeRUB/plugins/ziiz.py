44c9dc8db795bf73fb5416c56a81fb1c66b35315
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2026-02-13 10:01:42 +0300

    Update ziiz.py

--- JoKeRUB/plugins/ziiz.py
+++ JoKeRUB/plugins/ziiz.py
@@ -1,13 +1,43 @@
-
 import json
+import math
+import os
+import aiohttp
 import requests
-from telethon.tl.functions.users import GetUsersRequest
+import random
+import re
+import time
+from uuid import uuid4
+import sys
+import asyncio
+from validators.url import url
+from subprocess import run as runapp
+from datetime import datetime
+from pySmartDL import SmartDL
+from pathlib import Path
+from platform import python_version
+from telethon import Button, functions, events, types, custom
+from telethon.errors import QueryIdInvalidError
+from telethon.events import CallbackQuery, InlineQuery
+from telethon.utils import get_display_name
+from telethon.tl.types import InputMessagesFilterDocument
 from telethon.tl.types import MessageEntityMentionName
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import BotInlineResult, InputBotInlineMessageMediaAuto, DocumentAttributeImageSize, InputWebDocument, InputBotInlineResult
+from telethon.tl.functions.messages import SetInlineBotResultsRequest
 
 from . import l313l
 from ..Config import Config
-from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+from ..helpers.functions import rand_key
 from ..core.logger import logging
+from ..core.managers import edit_delete, edit_or_reply
+from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+from ..helpers.utils import reply_id, _format
+from . import media_type, progress
+from ..utils import Zed_Dev, load_module, remove_plugin
+from ..sql_helper.global_collection import add_to_collectionlist, del_keyword_collectionlist, get_collectionlist_items
+from . import SUDO_LIST, edit_delete, edit_or_reply, reply_id, BOTLOG, BOTLOG_CHATID, HEROKU_APP, mention
+
 
 LOGS = logging.getLogger(__name__)
 
@@ -117,3 +147,4 @@ async def repozedub(event):
     except Exception as e:
         await event.edit("❌ حدث خطأ، حاول مرة أخرى")
         LOGS.error(f"❌ خطأ: {e}")
+        

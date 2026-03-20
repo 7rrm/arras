861de7a1eaed3b5af932535932a57119eb281c51
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2026-03-20 20:03:37 +0300

    Update التخزين.py

--- JoKeRUB/plugins/التخزين.py
+++ JoKeRUB/plugins/التخزين.py
@@ -25,8 +25,8 @@ class LOG_CHATS:
 
 LOG_CHATS_ = LOG_CHATS()
 
-@l313l.ar_cmd(incoming=True, func=lambda e: e.is_private, edited=False, forword=None)
-async def monito_p_m_s(event):
+@l313l.ar_cmd(incoming=True, func=lambda e: e.is_private, edited=False, forward=None)  # تم تصحيح forword إلى forward
+async def monito_p_m_s(event):  # sourcery no-metrics
     if Config.PM_LOGGER_GROUP_ID == -100:
         return
     if gvarstatus("PMLOG") and gvarstatus("PMLOG") == "false":
@@ -34,42 +34,27 @@ async def monito_p_m_s(event):
     sender = await event.get_sender()
     if not sender.bot:
         chat = await event.get_chat()
+        fullname = f"{sender.first_name}{sender.last_name}" if sender.last_name else sender.first_name #Write Code By T.me/ZThon
+        user_name = f"@{sender.username}" if sender.username else "لا يوجـد" #Write Code By T.me/ZThon
         if not no_log_pms_sql.is_approved(chat.id) and chat.id != 777000:
             if LOG_CHATS_.RECENT_USER != chat.id:
                 LOG_CHATS_.RECENT_USER = chat.id
                 if LOG_CHATS_.NEWPM:
-                    if LOG_CHATS_.COUNT > 1:
-                        try:
-                            await LOG_CHATS_.NEWPM.edit(
-                                LOG_CHATS_.NEWPM.text.replace(
-                                    " **📮┊رسـاله جـديده**", f"{LOG_CHATS_.COUNT} **رسـائل**"
-                                )
-                            )
-                        except Exception as er:
-                            LOGS.error(f"Error: {er}")
-                    else:
-                        await event.client.send_message(
-                            Config.PM_LOGGER_GROUP_ID,
-                            LOG_CHATS_.NEWPM.text.replace(
-                                " **📮┊رسـاله جـديده**", f"{LOG_CHATS_.COUNT} **رسـائل**"
-                            )
-                        )
                     LOG_CHATS_.COUNT = 0
                 LOG_CHATS_.NEWPM = await event.client.send_message(
                     Config.PM_LOGGER_GROUP_ID,
-                    f"**🛂┊المسـتخـدم :** {_format.mentionuser(sender.first_name , sender.id)} **- قام بـ إرسـال رسـالة جـديـده** \n**🎟┊الايـدي :** `{chat.id}`",
+                    f"**🚹┊المسـتخـدم :** {_format.mentionuser(fullname, sender.id)} .\n**🎟┊الايـدي :** `{chat.id}`\n**🌀┊اليـوزر :** {user_name}\n\n**💌┊قام بـ إرسـال رسائـل جـديـده**",
                 )
             try:
                 if event.message:
                     forwarded_msg = await event.client.forward_messages(
                         Config.PM_LOGGER_GROUP_ID, event.message, silent=True
                     )
-                    # تخزين الرسالة المحولة والرسالة الأصلية
                     LOG_CHATS_.STORED_MESSAGES[event.message.id] = forwarded_msg.id
                     LOG_CHATS_.ORIGINAL_MESSAGES[event.message.id] = event.message.text
                 LOG_CHATS_.COUNT += 1
             except Exception as e:
-                LOGS.error(f"Error: {e}")
+                LOGS.warn(str(e))
 
 @l313l.ar_cmd(incoming=True, func=lambda e: e.mentioned, edited=False, forword=None)
 async def log_tagged_messages(event):

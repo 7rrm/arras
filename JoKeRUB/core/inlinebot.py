0764cb34e183672c2f83b4597820d855971b6a97
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2026-03-26 15:46:39 +0300

    Update inlinebot.py

--- JoKeRUB/core/inlinebot.py
+++ JoKeRUB/core/inlinebot.py
@@ -10,9 +10,9 @@ from telethon import Button, types, version
 from telethon.errors import QueryIdInvalidError
 from telethon.events import CallbackQuery, InlineQuery
 from youtubesearchpython import VideosSearch
-from zlzl import zedub, zedversion, StartTime
+from JoKeRUB import l313l, catversion, StartTime
 from ..Config import Config
-from ..helpers.functions import rand_key, zedalive, check_data_base_heal_th, get_readable_time
+from ..helpers.functions import rand_key, catalive, check_data_base_heal_th, get_readable_time
 from ..helpers.functions.utube import download_button, get_yt_video_id, get_ytthumb, result_formatter, ytsearch_data
 from ..plugins import mention
 from ..sql_helper.globals import gvarstatus

547b14aae489070ae7e079174ad81bff72df528f
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2026-03-28 00:02:52 +0300

    Update startup.py

--- JoKeRUB/utils/startup.py
+++ JoKeRUB/utils/startup.py
@@ -217,7 +217,11 @@ async def startupmessage():
         if msg_details:
             await l313l.check_testcases()
             message = await l313l.get_messages(msg_details[0], ids=msg_details[1])
-            text = message.text + "\n\n**تم تشغيل البوت الأن أرسل `.فحص`**"
+            text = f"ᯓ 𝗦𝗢𝗨𝗥𝗖𝗘 𝗔𝗥𝗥𝗔𝗦 🝢 **تـم الـتـحـديـث**\n" \
+       f"**•─────────────────•**\n" \
+       f"**•⎆┊اهـلا عـزيـزي** - {mention}\n" \
+       f"**•⎆┊تم تحديث سورس آراس بنجاح**\n\n" \
+       f"**تم تشغيل البوت الأن أرسل `.فحص`**"
             await l313l.edit_message(msg_details[0], msg_details[1], text)
             if gvarstatus("restartupdate") is not None:
                 await l313l.send_message(

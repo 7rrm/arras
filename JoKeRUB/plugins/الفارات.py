b7c35d66c45fbff92beb756c16bdee92c46ddb6c
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2026-03-30 23:47:05 +0300

    Update الفارات.py

--- JoKeRUB/plugins/الفارات.py
+++ JoKeRUB/plugins/الفارات.py
@@ -8,10 +8,6 @@ import urllib3
 from telethon import events
 from JoKeRUB import l313l
 
-from .utils import (
-    add_bot_to_logger_group,
-    install_externalrepo,
-)
 
 from ..Config import Config
 from ..core.managers import edit_delete, edit_or_reply
@@ -678,67 +674,41 @@ async def variable(event):
             await jep.edit("**⌔∮ تم بنجاح تغيير زخرفة الصورة الوقتية\n\n❃ جار اعادة تشغيل السورس انتظر من 2-5 دقائق ليتشغل مره اخرى**".format(input_str))
         heroku_var[variable] = zinfo
         '''
-
 @l313l.ar_cmd(pattern="ميوزك(?:\s|$)([\s\S]*)")
 async def variable(event):
     if Config.HEROKU_API_KEY is None:
         return await edit_delete(
             event,
-            "اضبط Var المطلوب في Heroku على وظيفة هذا بشكل طبيعي `HEROKU_API_KEY` اذا كنت لاتعلم اين يوجد فقط اذهب الى حسابك في هيروكو ثم الى الاعدادات ستجده بالاسفل انسخه ودخله في الفار.",
+            "اضبط Var المطلوب في Heroku على وظيفة هذا بشكل طبيعي `HEROKU_API_KEY` اذا كنت لاتعلم اين يوجد فقط اذهب الى حسابك في هيروكو ثم الى الاعدادات ستجده بالاسفل انسخه ودخله في الفار. ",
         )
     if Config.HEROKU_APP_NAME is not None:
         app = Heroku.app(Config.HEROKU_APP_NAME)
     else:
-        return await edit_delete(
+        return await ed(
             event,
             "اضبط Var المطلوب في Heroku على وظيفة هذا بشكل طبيعي `HEROKU_APP_NAME` اسم التطبيق اذا كنت لاتعلم.",
         )
-    
     input_str = event.pattern_match.group(1)
     heroku_var = app.config()
     jep = await edit_or_reply(event, "** جارِ تغير وضع الميوزك ✅ . . .**")
-    
     if input_str == "تفعيل":
         variable = "VCMODE"
         zinfo = "True"
         await asyncio.sleep(1.5)
+        if variable in heroku_var:
+            await jep.edit("**⌔∮ تم بنجاح تغيير وضع الميوزك\n\n❃ جار اعادة تشغيل السورس انتظر من 2-5 دقائق ليتشغل مره اخرى**".format(input_str))
+        else:
+            await jep.edit("**⌔∮ تم بنجاح تغيير وضع الميوزك\n\n❃ جار اعادة تشغيل السورس انتظر من 2-5 دقائق ليتشغل مره اخرى**".format(input_str))
         heroku_var[variable] = zinfo
-        await jep.edit("**⌔∮ تم بنجاح تغيير وضع الميوزك\n\n❃ جار اعادة تشغيل السورس انتظر من 2-5 دقائق ليتشغل مره اخرى**")
-        
     elif input_str == "تعطيل":
         variable = "VCMODE"
         zinfo = "False"
         await asyncio.sleep(1.5)
+        if variable in heroku_var:
+            await jep.edit("**⌔∮ تم بنجاح تغيير وضع الميوزك\n\n❃ جار اعادة تشغيل السورس انتظر من 2-5 دقائق ليتشغل مره اخرى**".format(input_str))
+        else:
+            await jep.edit("**⌔∮ تم بنجاح تغيير وضع الميوزك\n\n❃ جار اعادة تشغيل السورس انتظر من 2-5 دقائق ليتشغل مره اخرى**".format(input_str))
         heroku_var[variable] = zinfo
-        await jep.edit("**⌔∮ تم بنجاح تغيير وضع الميوزك\n\n❃ جار اعادة تشغيل السورس انتظر من 2-5 دقائق ليتشغل مره اخرى**")
-    
-    elif input_str == "تحديث":
-        # نسبة 20%
-        zzz1 = await jep.edit("ᯓ 𝗦𝗢𝗨𝗥𝗖𝗘 𝗔𝗥𝗥𝗔𝗦 🝢 **تـحـديـث مـيـوزك**\n**•─────────────────•**\n**⇜ جـارِ تـحـديـث مـيـوزك اراس . . .🌐**\n\n%𝟸𝟶 ▬▬▭▭▭▭▭▭▭▭")
-        await asyncio.sleep(1)
-        
-        # نسبة 50%
-        zzz2 = await zzz1.edit("ᯓ 𝗦𝗢𝗨𝗥𝗖𝗘 𝗔𝗥𝗥𝗔𝗦 🝢 **تـحـديـث مـيـوزك**\n**•─────────────────•**\n**⇜ جـارِ تـحـديـث مـيـوزك اراس . . .🌐**\n\n%𝟻𝟶 ▬▬▬▬▬▭▭▭▭▭")
-        await asyncio.sleep(1)
-        
-        # نسبة 80%
-        zzz3 = await zzz2.edit("ᯓ 𝗦𝗢𝗨𝗥𝗖𝗘 𝗔𝗥𝗥𝗔𝗦 🝢 **تـحـديـث مـيـوزك**\n**•─────────────────•**\n**⇜ جـارِ تـحـديـث مـيـوزك اراس . . .🌐**\n\n%𝟾𝟶 ▬▬▬▬▬▬▬▬▭▭")
-        await asyncio.sleep(1)
-        
-        # نسبة 100%
-        zzz4 = await zzz3.edit("ᯓ 𝗦𝗢𝗨𝗥𝗖𝗘 𝗔𝗥𝗥𝗔𝗦 🝢 **تـحـديـث مـيـوزك**\n**•─────────────────•**\n**⇜ جـارِ تـحـديـث مـيـوزك اراس . . .🌐**\n\n%𝟷𝟶𝟶 ▬▬▬▬▬▬▬▬▬▬ 💯")
-        await asyncio.sleep(1)
-        
-        # تحديث مستودع الميوزك
-        try:
-            await install_externalrepo("https://github.com/jepthoniq/JepVc", "jepvc", "jepthonvc")
-            await zzz4.edit("ᯓ 𝗦𝗢𝗨𝗥𝗖𝗘 𝗔𝗥𝗥𝗔𝗦 🝢 **تـم تـحـديـث مـيـوزك**\n**•─────────────────•**\n**•⎆┊تم تحديث ميوزك اراس بنجاح ✅**")
-        except Exception as e:
-            await zzz4.edit(f"**⌔∮ فشل تحديث مستودع الميوزك:\n`{str(e)}`**")
-            return
-    
-    else:
-        return await edit_delete(event, "**الاستخدام الصحيح:\n`.ميوزك تفعيل`\n`.ميوزك تعطيل`\n`.ميوزك تحديث`**")
 
 @l313l.ar_cmd(pattern="استخدامي$")
 async def dyno_usage(dyno):

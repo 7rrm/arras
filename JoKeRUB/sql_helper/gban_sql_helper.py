711055047ce2803aafe5366ba252799cc621e2bf
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2026-04-15 19:45:52 +0300

    Update gban_sql_helper.py

--- JoKeRUB/sql_helper/gban_sql_helper.py
+++ JoKeRUB/sql_helper/gban_sql_helper.py
@@ -1,17 +1,3 @@
-"""
-credits to @mrconfused and @lMl10l
-"""
-#    Copyright (C) 2020  sandeep.n(π.$)
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#   You should have received a copy of the GNU Affero General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from sqlalchemy import Column, String
 
 from . import BASE, SESSION
@@ -27,7 +13,7 @@ class GBan(BASE):
         self.reason = reason
 
 
-GBan.__table__.create(checkfirst=True)
+GBan.__table__.create(bind=SESSION.get_bind(), checkfirst=True)
 
 
 def is_gbanned(chat_id):
@@ -46,7 +32,7 @@ def get_gbanuser(chat_id):
         SESSION.close()
 
 
-def catgban(chat_id, reason):
+def zedgban(chat_id, reason):
     adder = GBan(str(chat_id), str(reason))
     SESSION.add(adder)
     SESSION.commit()

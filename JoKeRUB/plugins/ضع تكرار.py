e0ff3a0ee70c1e47f64820c683a7e2e77966969d
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2026-04-16 10:35:45 +0300

    Update ضع تكرار.py

--- JoKeRUB/plugins/ضع تكرار.py
+++ JoKeRUB/plugins/ضع تكرار.py
@@ -5,9 +5,8 @@ from telethon.tl.types import ChatBannedRights
 
 from ..sql_helper import antiflood_sql as sql
 from ..utils import is_admin
-from . import l313l, edit_or_reply
+from . import edit_or_reply, l313l
 
-plugin_category = "admin"
 CHAT_FLOOD = sql.__load_flood_settings()
 
 ANTI_FLOOD_WARN_MODE = ChatBannedRights(
@@ -19,8 +18,8 @@ ANTI_FLOOD_WARN_MODE = ChatBannedRights(
 async def _(event):
     if not CHAT_FLOOD:
         return
-    catadmin = await is_admin(event.client, event.chat_id, event.client.uid)
-    if not catadmin:
+    zthonadmin = await is_admin(event.client, event.chat_id, event.client.uid)
+    if not zthonadmin:
         return
     if str(event.chat_id) not in CHAT_FLOOD:
         return
@@ -36,50 +35,34 @@ async def _(event):
     except Exception as e:
         no_admin_privilege_message = await event.client.send_message(
             entity=event.chat_id,
-            message=f"""**بـوت مـانع الـتكرار**
-@admin [المستخدم](tg://user?id={event.message.sender_id}) قـام بالتكرار في هذه الدردشه.
-`{str(e)}`""",
+            message=f"ᯓ 𝗦𝗢𝗨𝗥𝗖𝗘 𝗔𝗥𝗥𝗔𝗦 - **مانع التكرار**\n━━━━━━━━━━━━━━━━━━━━\n\n**✧╎ قام↫** [المستخدم](tg://user?id={event.message.sender_id})\n**✧╎بتكرار رسائله في المجموعة**\x1f`{e}`",
             reply_to=event.message.id,
         )
+
         await asyncio.sleep(4)
         await no_admin_privilege_message.edit(
-            "** هذا التكرار ما يفيدك اخي استمتع بالدردشة مثل الناس 🧸💞** "
+            "**✧╎هذا هو الشخص الذي قام بالتكرار \n✧╎توقف لكي لا تًطرد 📵**"
         )
     else:
         await event.client.send_message(
             entity=event.chat_id,
-            message=f"""**بـوت مـانع الـتكرار**
-[المستخدم](tg://user?id={event.message.sender_id}) \n قـام بتجاوز عدد الـتكرار
-لـذلك تـم تقيـيده""",
+            message=f"ᯓ 𝗦𝗢𝗨𝗥𝗖𝗘 𝗔𝗥𝗥𝗔𝗦 - **مانع التكرار**\n━━━━━━━━━━━━━━━━━━━━\n\n**✧╎قام ↫**[المستخدم ](tg://user?id={event.message.sender_id})\n**✧╎بتجاوز عدد الـتكرار لـذلك تـم تقيـيده**",
             reply_to=event.message.id,
         )
 
 
 @l313l.ar_cmd(
-    pattern="ضع تكرار(?: |$)(.*)",
-    command=("ضع تكرار", plugin_category),
-    info={
-        "header": "To setup antiflood in a group",
-        "description": "It warns the user if he spams the chat and if you are an admin with proper rights then it mutes him in that group.",
-        "note": "To stop antiflood setflood with high value like 999999",
-        "usage": "{tr}setflood <count>",
-        "examples": [
-            "{tr}setflood 10",
-        ],
-    },
+    pattern="ضع تكرار(?:\s|$)([\s\S]*)",
     groups_only=True,
     require_admin=True,
 )
 async def _(event):
-    "To setup antiflood in a group to prevent spam"
     input_str = event.pattern_match.group(1)
-    event = await edit_or_reply(event, "** تم تحديث عدد التكرار 🧸♥**")
+    event = await edit_or_reply(event, "**✧╎تم تحديث عدد التكرار ..بنجـاح**")
     await asyncio.sleep(2)
     try:
         sql.set_flood(event.chat_id, input_str)
         sql.__load_flood_settings()
-        await event.edit(f"تم تحديث عدد الـتكرار لـ {input_str} في الدردشه الحالية")
+        await event.edit(f"**✧╎تم تحديث التكرار الى {input_str} في الدردشة الحالية**")
     except Exception as e:
         await event.edit(str(e))
-# For Catuserbot
-# Arabic Translate By  :  @lMl10l

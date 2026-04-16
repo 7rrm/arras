9879fe18685733d816f3796ade79c86fb8fd87a3
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2026-04-16 22:15:22 +0300

    Change button style from primary to danger

--- JoKeRUB/plugins/znz.py
+++ JoKeRUB/plugins/znz.py
@@ -99,7 +99,7 @@ async def inline_handler(event):
             new_msg = {
                 str(timestamp): {"userid": user_list, "text": query}
             }  # Code by T.me/zzzzl1l
-            buttons = [[Button.inline(info_type[2], data=f"{scc}_{timestamp}", style="primary")]]
+            buttons = [[Button.inline(info_type[2], data=f"{scc}_{timestamp}", style="danger")]]
             result = builder.article(
                 title=f"{hmm} {zilzal}",
                 description=f"{dss}",
@@ -165,7 +165,7 @@ async def inline_handler(event):
             new_msg = {
                 str(timestamp): {"userid": user_list, "text": query}
             }  # Code by T.me/zzzzl1l
-            buttons = [[Button.inline(info_type[2], data=f"{scc}_{timestamp}", style="primary")]]
+            buttons = [[Button.inline(info_type[2], data=f"{scc}_{timestamp}", style="danger")]]
             result = builder.article(
                 title=f"{hmm} {zilzal}",
                 description=f"{dss}",

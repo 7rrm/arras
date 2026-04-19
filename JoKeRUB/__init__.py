766cc59e85f3569729f60f083998be750a4378b0
7rrm <krardh17@gmail.com>
2026-04-19 10:31:26 +0300

    Update __init__.py

--- JoKeRUB/__init__.py
+++ JoKeRUB/__init__.py
@@ -5,6 +5,7 @@ import heroku3
 from .Config import Config
 from .core.logger import logging
 from .core.session import l313l
+from .helpers.functions.converter import Convert
 from .sql_helper.globals import addgvar, delgvar, gvarstatus
 
 __version__ = "3.1.3"

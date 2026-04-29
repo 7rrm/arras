e658fb6ba6b83e0f176e2d16e31c1209c4c35c5b
7rrm <krardh17@gmail.com>
2026-04-29 19:40:35 +0300

    Update id.py

--- JoKeRUB/assistant/id.py
+++ JoKeRUB/assistant/id.py
@@ -4,4 +4,10 @@ from telethon.utils import pack_bot_file_id
 @tgbot.on(events.NewMessage(pattern="^الايدي"))
 async def _(e):
     r = await e.get_reply_message()
-    await e.reply(f"الدردشة: {e.chat_id}" + (f"\nالمستخدم: {r.sender_id}" + (f"\nنوع الميديا: {type(r.media).__name__}" if r and r.media else "") if r else ""))
+    if r:
+        txt = f"الدردشة: {e.chat_id}\nالمستخدم: {r.sender_id}"
+        if r.media:
+            txt += f"\nالميديا: {pack_bot_file_id(r.media)}"
+        await e.reply(txt)
+    else:
+        await e.reply(f"الدردشة: {e.chat_id}")

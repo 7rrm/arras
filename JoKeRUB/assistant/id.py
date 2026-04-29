78bbdbbc8c7c8cc72830056a703030c948b4bfb5
7rrm <krardh17@gmail.com>
2026-04-29 19:35:57 +0300

    Update id.py

--- JoKeRUB/assistant/id.py
+++ JoKeRUB/assistant/id.py
@@ -1,29 +1,19 @@
 from telethon import events
 from telethon.utils import pack_bot_file_id
 
-
 @tgbot.on(events.NewMessage(pattern="^الايدي"))
 async def _(event):
-    if event.reply_to_msg_id:
-        await event.get_input_chat()
-        r_msg = await event.get_reply_message()
-        if r_msg.media:
-            bot_api_file_id = pack_bot_file_id(r_msg.media)
-            await tgbot.send_message(
-                event.chat_id,
-                "⌯︙ايـدي الـدردشة: `{}`\n⌯︙ايدي المستخدم: `{}`".format(
-                    str(event.chat_id), str(r_msg.from_id), bot_api_file_id
-                ),
-            )
-        else:
-            await tgbot.send_message(
-                event.chat_id,
-                "⌯︙ايـدي الـدردشة: `{}`\n⌯︙ايدي المستخدم: `{}`".format(
-                    str(event.chat_id), str(r_msg.from_id)
-                ),
-            )
+    chat_id = event.chat_id
+    reply_msg = await event.get_reply_message()
+    
+    if reply_msg:
+        user_id = reply_msg.sender_id
+        msg = f"⌯︙ايـدي الـدردشة: `{chat_id}`\n⌯︙ايدي المستخدم: `{user_id}`"
+        
+        if reply_msg.media:
+            bot_api_file_id = pack_bot_file_id(reply_msg.media)
+            msg += f"\n⌯︙ايـدي الميديا: `{bot_api_file_id}`"
     else:
-        await tgbot.send_message(
-            event.chat_id, "⌯︙ايـدي الـدردشة: `{}`".format(str(event.chat_id))
-        )
-
+        msg = f"⌯︙ايـدي الـدردشة: `{chat_id}`"
+    
+    await event.reply(msg)

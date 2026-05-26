324fe9ff6b4e2cee3e96208078a01b3c8cd82ba8
7rrm <krardh17@gmail.com>
2026-05-26 15:23:34 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -1,18 +1,16 @@
 from sample_config import Config
 
 class Development(Config):
-    # get this values from the my.telegram.org
+    
     APP_ID = 21623560
     API_HASH = "8c448c687d43262833a0ab100255fb43"
-    # the name to display in your alive message
     ALIVE_NAME = "K"
-    # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "رابط قاعدة البيانات"
-    # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
     STRING_SESSION = "كود التيرمكس"
-    # create a new bot in @botfather and fill the following vales with bottoken and username respectively
     TG_BOT_TOKEN = "توكن البوت"
-    # command handler
+    GROQ_API_KEY = "توكن الذكاء"
+ 
+    # command handler - اتركهن 
     COMMAND_HAND_LER = "."
     # sudo enter the id of sudo users userid's in that array
     SUDO_USERS = []
